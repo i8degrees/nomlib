f79b99a5c660b016e595aaa0fdd8781353899cca
Jeffrey Carpenter <jeffrey.carp@gmail.com>
2013-05-05 09:24:48 -0500

    Additional Groundwork / Foundation

--- src/input.cpp
+++ src/input.cpp
@@ -8,20 +8,108 @@
 ******************************************************************************/
 #include "input.h"
 
-SDLInput::SDLInput ( void )
+void SDLInput::HandleInput ( SDL_Event *input )
 {
-  #ifdef DEBUG_INPUT_OBJ
-    std::cout << "SDLInput::SDLInput(): Hello, world!" << std::endl << std::endl;
-  #endif
 
-  // Init
+  switch ( input->type )
+  {
+    case SDL_QUIT:
+      onExit();
+    break;
+
+    case SDL_VIDEORESIZE:
+      onResize();
+    break;
+
+    case SDL_KEYDOWN:
+      keyDown ( input->key.keysym.sym, input->key.keysym.mod );
+    break;
+
+    case SDL_KEYUP:
+      keyUp ( input->key.keysym.sym, input->key.keysym.mod );
+    break;
+
+    case SDL_MOUSEMOTION:
+      mouseMotion ( input->motion.x, input->motion.y );
+    break;
+
+    case SDL_MOUSEBUTTONDOWN:
+      switch ( input->button.button )
+      {
+        case SDL_BUTTON_LEFT:
+          mouseLeftButtonDown ( input->button.x, input->button.y );
+        break;
+
+        case SDL_BUTTON_MIDDLE:
+          mouseMiddleButtonDown ( input->button.x, input->button.y );
+        break;
+
+        case SDL_BUTTON_RIGHT:
+          mouseRightButtonDown ( input->button.x, input->button.y );
+        break;
+      }
+    break;
+
+    case SDL_MOUSEBUTTONUP:
+      switch ( input->button.button )
+      {
+        case SDL_BUTTON_LEFT:
+          mouseLeftButtonUp ( input->button.x, input->button.y );
+        break;
+
+        case SDL_BUTTON_MIDDLE:
+          mouseMiddleButtonUp ( input->button.x, input->button.y );
+        break;
+
+        case SDL_BUTTON_RIGHT:
+          mouseRightButtonUp ( input->button.x, input->button.y );
+        break;
+      }
+    break;
+  } // end switch input->type
+
 }
 
-SDLInput::~SDLInput ( void )
+void SDLInput::onExit ( void )
 {
-  #ifdef DEBUG_INPUT_OBJ
-    std::cout << "SDLInput::~SDLInput(): " << "Goodbye cruel world!" << std::endl << std::endl;
-  #endif
+}
 
-  // Cleanup of vars, etc.
+void SDLInput::onResize ( void )
+{
+}
+
+void SDLInput::keyDown ( SDLKey key, SDLMod mod )
+{
+}
+
+void SDLInput::keyUp ( SDLKey key, SDLMod mod )
+{
+}
+
+void SDLInput::mouseMotion ( unsigned int x, unsigned int y )
+{
+}
+
+void SDLInput::mouseLeftButtonDown ( unsigned int x, unsigned int y )
+{
+}
+
+void SDLInput::mouseMiddleButtonDown ( unsigned int x, unsigned int y )
+{
+}
+
+void SDLInput::mouseRightButtonDown ( unsigned int x, unsigned int y )
+{
+}
+
+void SDLInput::mouseLeftButtonUp ( unsigned int x, unsigned int y )
+{
+}
+
+void SDLInput::mouseMiddleButtonUp ( unsigned int x, unsigned int y )
+{
+}
+
+void SDLInput::mouseRightButtonUp ( unsigned int x, unsigned int y )
+{
 }
--- src/input.h
+++ src/input.h
@@ -17,11 +17,26 @@
 class SDLInput
 {
   public:
-    SDLInput ( void );
-    ~SDLInput ( void );
+    //SDLInput ( void );
+    virtual ~SDLInput ( void );
+
+    virtual void HandleInput ( SDL_Event *input );
+
+    virtual void onExit ( void );
+    virtual void onResize ( void );
+
+    virtual void keyDown ( SDLKey key, SDLMod mod );
+    virtual void keyUp ( SDLKey key, SDLMod mod );
+    virtual void mouseMotion ( unsigned int x, unsigned int y );
+    virtual void mouseLeftButtonDown ( unsigned int x, unsigned int y );
+    virtual void mouseMiddleButtonDown ( unsigned int x, unsigned int y );
+    virtual void mouseRightButtonDown ( unsigned int x, unsigned int y );
+    virtual void mouseLeftButtonUp ( unsigned int x, unsigned int y );
+    virtual void mouseMiddleButtonUp ( unsigned int x, unsigned int y );
+    virtual void mouseRightButtonUp ( unsigned int x, unsigned int y );
 
   private:
-    SDL_Event input; // events struct for input subsystem
+    //
 };
 
 #endif // GAMELIB_INPUT_HEADERS defined

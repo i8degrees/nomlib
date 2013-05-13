fa34a7cecc4a968863f55164ae9f9fea26fa4473
Jeffrey Carpenter <jeffrey.carp@gmail.com>
2013-05-13 18:06:56 -0500

    Initial Implementation (Stub) of SDLDrawables Abstract Virtual Interface

---
+++ src/SDLDrawable.h
@@ -0,0 +1,55 @@
+/******************************************************************************
+    SDLDrawable.h
+
+  Copyright (c) 2013 Jeffrey Carpenter
+  All rights reserved.
+
+******************************************************************************/
+#ifndef GAMELIB_SDL_DRAWABLE_HEADERS
+#define GAMELIB_SDL_DRAWABLE_HEADERS
+
+#include <iostream>
+#include <string>
+
+#include "SDL.h"
+
+#include "GCoords.h"
+
+#define DEBUG_SDL_DRAWABLE
+#define DEBUG_SDL_DRAWABLE_OBJ
+
+/*
+
+  std::list<SDLDrawable*> drawables;
+
+class SDLDrawable
+{
+  public:
+    // ...
+  private:
+    // ...
+};
+class SDLGradient: public SDLDrawable
+{
+  public:
+    // ...
+  private:
+    // ...
+};
+class SDLFont: public SDLDrawable
+{
+  public:
+    // ...
+  private:
+    // ...
+};
+
+*/
+
+class SDLDrawable
+{
+  public:
+    virtual void Draw ( SDL_Surface *video_buffer ) = 0;
+};
+
+#endif // GAMELIB_SDL_DRAWABLE_HEADERS defined

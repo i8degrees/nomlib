4c025c5099ce2eddeb082ce36492f9812e361ad2
Jeffrey Carpenter <jeffrey.carp@gmail.com>
2013-05-13 10:52:08 -0500

    Finished First Implementation of (Linear) Gradient Class

--- src/SDLGradient.cpp
+++ src/SDLGradient.cpp
@@ -12,11 +12,95 @@ SDLGradient::SDLGradient ( void )
   #ifdef DEBUG_GRADIENT_OBJ
     std::cout << "SDLGradient::SDLGradient (): " << "Hello, world!" << std::endl << std::endl;
   #endif
+
+  this->gradient[0].setColor ( 0, 0, 0 ); // starting color defaults
+  this->gradient[1].setColor ( 255, 255, 255 ); // ending color defaults
+
+  this->coords.setCoords ( 0, 0, 0, 0 ); // zero out dem blitting coords
+
+  this->x_margin = 0;
+  this->y_margin = 0;
+}
+
+void SDLGradient::Init (  GColor starting_color, GColor ending_color,
+                          unsigned int x, unsigned int y,
+                          unsigned int width, unsigned int height,
+                          unsigned int x_margin, unsigned int y_margin )
+{
+  #ifdef DEBUG_GRADIENT_OBJ
+    std::cout << "SDLGradient::SDLGradient (): " << "Hello, world!" << std::endl << std::endl;
+  #endif
+
+  this->gradient[0] = starting_color;
+  this->gradient[1] = ending_color;
+
+  this->coords.setCoords ( x, y, width, height ); // zero dem blitting coords
+
+  this->x_margin = x_margin; // offsets += 3 unless initialized in args list or not calling Init
+  this->y_margin = y_margin; // offsets += 4 unless initialized in args list or not calling Init
 }
 
 SDLGradient::~SDLGradient ( void )
 {
-  #ifdef DEBUG_COORDS_OBJ
+  #ifdef DEBUG_GRADIENT_OBJ
     std::cout << "SDLGradient::~SDLGradient (): " << "Goodbye cruel world!" << std::endl << std::endl;
   #endif
+}
+
+void SDLGradient::LinearFill (  SDL_Surface *video_buffer, unsigned int x, unsigned int y,
+                                unsigned int width, unsigned int height,
+                                unsigned int direction
+                              )
+{
+  unsigned int x_offset = x + width;
+  //unsigned int y_offset = y + height;
+
+  // width should always be one (1) with this particular blit algorithm
+  this->coords.setCoords ( x, y, 1, height - this->y_margin );
+
+  #ifdef DEBUG_GRADIENT
+    std::cout << "SDLGradient::LinearGradientFill (): " << "Variable Dump" << std::endl << std::endl;
+
+    for ( int i = 0; i < 2; i++ )
+    {
+      std::cout << "\nRed[" << i << "]: " << this->gradient[i].getRed() << "\n";
+      std::cout << "\nGreen[" << i << "]: " << this->gradient[i].getGreen() << "\n";
+      std::cout << "\nBlue[" << i << "]: " << this->gradient[i].getBlue() << "\n";
+    }
+  #endif
+
+  float currentR = (float) gradient[0].getRed();
+  float currentG = (float) gradient[0].getGreen();
+  float currentB = (float) gradient[0].getBlue();
+
+  float destR = (float) ( gradient[1].getRed() - gradient[0].getRed() )      / ( float ) ( width - this->x_margin );
+  float destG = (float) ( gradient[1].getGreen() - gradient[0].getGreen() )  / ( float ) ( width - this->x_margin );
+  float destB = (float) ( gradient[1].getBlue() - gradient[0].getBlue() )    / ( float ) ( width - this->x_margin );
+
+  unsigned int rows = this->coords.getX();
+
+  for ( rows = x; rows < x_offset - this->x_margin; rows++ )
+  {
+    Gfx::DrawRectangle (  video_buffer, rows, this->coords.getY(),
+                          this->coords.getW(), this->coords.getH(),
+                          ( unsigned int ) currentR,
+                          ( unsigned int ) currentG,
+                          ( unsigned int ) currentB
+                        );
+
+    if ( direction == 0 )
+    {
+      currentR += destR;
+      currentG += destG;
+      currentB += destB;
+    }
+    else if ( direction == 1 ) // reversed
+    {
+      currentR -= destR;
+      currentG -= destG;
+      currentB -= destB;
+    }
+
+  } // end for coords blit loop
+
 }
--- src/SDLGradient.h
+++ src/SDLGradient.h
@@ -5,13 +5,19 @@
   All rights reserved.
 
 ******************************************************************************/
-#ifndef GAMELIB_GRADIENT_HEADERS
-#define GAMELIB_GRADIENT_HEADERS
+#ifndef GAMELIB_SDL_GRADIENT_HEADERS
+#define GAMELIB_SDL_GRADIENT_HEADERS
 
 #include <iostream>
 #include <string>
 
 #include "SDL.h"
+
+#include "GCoords.h"
+#include "gfx.h"
+
+//#define DEBUG_GRADIENT
+//#define DEBUG_GRADIENT_OBJ
 
 class SDLGradient
 {
@@ -19,9 +25,41 @@ class SDLGradient
     SDLGradient ( void );
     ~SDLGradient ( void );
 
-  private:
-    // ...
+    // gradient[0] = starting color
+    // gradient[1] = ending color
 
+    void Init ( GColor starting_color, GColor ending_color,
+                unsigned int x = 0, unsigned int y = 0,
+                unsigned int width = 0, unsigned int height = 0,
+                unsigned int x_margin = 3, unsigned int y_margin = 4 );
+                // These default margin offsets are used in TTcards Info Boxes
+                // If they are not set here, they default to zero (0)
+
+    // direction { 0 } = ending color to starting color
+    // direction { 1 } = starting color to ending color
+
+    void LinearFill (   SDL_Surface *video_buffer, unsigned int x, unsigned int y,
+                        unsigned int width, unsigned int height,
+                        unsigned int direction = 0
+                    );
+
+  private:
+    GColor gradient[2]; // holds R, G, B * 2
+    GCoords coords; // x, y, width, height coords
+    unsigned int x_margin; // x coordinate offset
+    unsigned int y_margin; // y coordinate offset
 };
 
-#endif // GAMELIB_GRADIENT_HEADERS defined
+/*
+
+  SDLGradient gradient_l;
+
+  GColor starting ( 66, 66, 66 );
+  GColor ending ( 99, 99, 99 );
+
+  gradient_l.Init ( starting, ending, 104, 194, 176, 24, 3, 4 );
+  gradient_l.LinearFill ( this->engine->screen, 104, 194, 176, 24, 0 );
+
+*/
+
+#endif // GAMELIB_SDL_GRADIENT_HEADERS defined

39acd34b29e2b78862a35e09a7ae3aaa846b45a8
Jeffrey Carpenter <jeffrey.carp@gmail.com>
2013-05-25 00:29:57 -0500

    Initial interfacing of SDL_Cursor class

---
+++ src/SDL_Cursor.cpp
@@ -0,0 +1,99 @@
+/******************************************************************************
+    SDL_Cursor.cpp
+
+  Helper class for loading, positioning and keeping track of a cursor controlled
+  by a mouse and / or keyboard.
+
+  Copyright (c) 2013 Jeffrey Carpenter
+
+******************************************************************************/
+#include "SDL_Cursor.h"
+
+nom::SDL_Cursor::SDL_Cursor ( void )
+{
+  #ifdef DEBUG_SDL_CURSOR_OBJ
+    std::cout << "SDL_Cursor::SDL_Cursor (): Hello, world!" << "\n" << std::endl;
+  #endif
+
+  // Not yet implemented
+}
+
+nom::SDL_Cursor::SDL_Cursor ( unsigned int x, unsigned int y, unsigned int width, unsigned int height )
+{
+  #ifdef DEBUG_SDL_CURSOR_OBJ
+    std::cout << "SDL_Cursor::SDL_Cursor (): Hello, world!" << "\n" << std::endl;
+  #endif
+
+  this->cursor = nom::Sprite ( width, height );
+  this->cursor.setXY ( x, y );
+  this->cursor.setState ( 0 ); // default state init
+}
+
+nom::SDL_Cursor::~SDL_Cursor ( void )
+{
+  #ifdef DEBUG_SDL_CURSOR_OBJ
+    std::cout << "SDL_Cursor::~SDL_Cursor (): " << "Goodbye cruel world!" << "\n" << std::endl;
+  #endif
+
+  // Clean up
+}
+
+void nom::SDL_Cursor::Load ( std::string filename, GColor colorkey ) // sheet_id ?
+{
+  this->cursor.Load ( filename, colorkey );
+}
+
+unsigned int nom::SDL_Cursor::getX ( void )
+{
+  return this->cursor.getX();
+}
+
+unsigned int nom::SDL_Cursor::getY ( void )
+{
+  return this->cursor.getY();
+}
+
+void nom::SDL_Cursor::setX ( unsigned int x )
+{
+  this->cursor.setX ( x );
+}
+
+void nom::SDL_Cursor::setY ( unsigned int y )
+{
+  this->cursor.setY ( y );
+}
+
+void nom::SDL_Cursor::updateXY ( unsigned int x, unsigned int y )
+{
+  this->cursor.updateXY ( x, y );
+}
+
+signed int nom::SDL_Cursor::getSheetID ( void )
+{
+  return this->cursor.getSheetID();
+}
+
+void nom::SDL_Cursor::setSheetID ( signed int sheet_id )
+{
+  this->cursor.setSheetID ( sheet_id );
+}
+
+unsigned int nom::SDL_Cursor::getState ( void )
+{
+  // Stub
+}
+
+void nom::SDL_Cursor::setState ( unsigned int state )
+{
+  // Stub
+}
+
+void nom::SDL_Cursor::Update ( void )
+{
+  // Stub
+}
+
+void nom::SDL_Cursor::Draw ( Gfx *engine /*SDL_Surface *video_buffer*/ )
+{
+  this->cursor.Draw ( engine );
+}
---
+++ src/SDL_Cursor.h
@@ -0,0 +1,71 @@
+/******************************************************************************
+    SDL_Cursor.h
+
+  Helper class for loading, positioning and keeping track of a cursor controlled
+  by a mouse and / or keyboard.
+
+  Copyright (c) 2013 Jeffrey Carpenter
+
+******************************************************************************/
+#ifndef NOMNOM_SDL_CURSOR_HEADERS
+#define NOMNOM_SDL_CURSOR_HEADERS
+
+#include <iostream>
+#include <string>
+
+#include "GCoords.h"
+#include "sprite.h"
+
+#include "gamelib.h"
+
+namespace NomNom
+{
+  class SDL_Cursor
+  {
+    public:
+      SDL_Cursor ( void );
+      ~SDL_Cursor ( void );
+
+      SDL_Cursor ( unsigned int x, unsigned int y, unsigned int width, unsigned int height );
+      //void Init ( void );
+      void Load ( std::string filename, GColor colorkey );
+
+      unsigned int getX ( void );
+      unsigned int getY ( void );
+      void setX ( unsigned int x );
+      void setY ( unsigned int y );
+      void updateXY ( unsigned int x = 0, unsigned int y = 0);
+
+      signed int getSheetID ( void );
+      void setSheetID ( signed int sheet_id = -1 );
+
+      unsigned int getState ( void );
+      void setState ( unsigned int state );
+      //bool isCursorLocked ( void );
+      //void lockCursor ( bool toggle );
+
+      GCoords getCursorPos ( unsigned int x, unsigned int y );
+      void moveCursorUp();
+      void moveCursorDown();
+      void moveCursorLeft();
+      void moveCursorRight();
+
+      void Update ( void );
+      void Draw ( Gfx *engine /*SDL_Surface *video_buffer*/ );
+
+    private:
+      nom::Sprite cursor; // interface cursor
+
+      // cursor XY coords mapping
+      std::vector<GCoords> coords_map;
+
+      //    coords_map schema:
+      //
+      //  [index-pos].[ x, y, width, height ]
+      //
+  };
+}
+
+namespace nom = NomNom; // alias declaration
+
+#endif // NOMNOM_SDL_CURSOR_HEADERS defined

ca464b489a4c9b0446a577821fac851853c427ff
Jeffrey Carpenter <jeffrey.carp@gmail.com>
2013-06-07 20:15:04 -0500

    Major refactoring / cleanup; new methods, etc.

--- src/SDL_Canvas.cpp
+++ src/SDL_Canvas.cpp
@@ -8,16 +8,18 @@
 ******************************************************************************/
 #include "SDL_Canvas.hpp"
 
-using namespace nom;
-
-SDL_Canvas::SDL_Canvas ( void ) : canvas_buffer ( NULL )
+nom::SDL_Canvas::SDL_Canvas ( void )  : canvas_buffer ( NULL ),
+                                        coords ( 0, 0, 0, 0 ),
+                                        offsets ( 0, 0, 0, 0 ),
+                                        //offsets ( -1, -1, -1, -1 ),
+                                        colorkey ( -1, -1, -1, -1 )
 {
   #ifdef DEBUG_SDL_CANVAS_OBJ
     std::cout << "nom::SDL_Canvas::SDL_Canvas(): Hello, world!" << "\n" << std::endl;
   #endif
 }
 
-SDL_Canvas::SDL_Canvas ( int32_t width, int32_t height, const Color& colors, uint32_t flags )
+nom::SDL_Canvas::SDL_Canvas ( int32_t width, int32_t height, const Color& colors, uint32_t flags )
 {
   #ifdef DEBUG_SDL_CANVAS_OBJ
     std::cout << "nom::SDL_Canvas::SDL_Canvas(): Hello, world!" << "\n" << std::endl;
@@ -26,59 +28,91 @@ SDL_Canvas::SDL_Canvas ( int32_t width, int32_t height, const Color& colors, uin
   // ...Create a new surface here
 }
 
-SDL_Canvas::~SDL_Canvas ( void )
+nom::SDL_Canvas::SDL_Canvas ( void* video_buffer )
+{
+  this->canvas_buffer = (SDL_Surface*) video_buffer;
+}
+
+nom::SDL_Canvas::~SDL_Canvas ( void )
 {
   #ifdef DEBUG_SDL_CANVAS_OBJ
     std::cout << "nom::SDL_Canvas::~SDL_Canvas(): " << "Goodbye cruel world!" << "\n" << std::endl;
   #endif
 
-  //if ( this->canvas_buffer != NULL )
-    //SDL_FreeSurface ( canvas_buffer );
+  if ( this->canvas_buffer != NULL )
+    SDL_FreeSurface ( canvas_buffer );
 
-  //this->canvas_buffer = NULL;
+  this->canvas_buffer = NULL;
 }
 
-void SDL_Canvas::setCanvas ( const Surface<SDL_Surface>& video_buffer )
+void* nom::SDL_Canvas::get ( void )
 {
-  canvas_buffer = video_buffer;
+  return this->canvas_buffer;
 }
 
-bool SDL_Canvas::loadImage ( std::string filename, const Color& colorkey, unsigned int flags )
+void nom::SDL_Canvas::setCanvas ( SDL_Surface *video_buffer )
 {
-  canvas_buffer = (SDL_Surface*) Gfx::LoadImage ( filename, colorkey, flags );
+  this->canvas_buffer = video_buffer;
+}
+
+void nom::SDL_Canvas::setCanvas ( void* video_buffer )
+{
+  this->canvas_buffer = (SDL_Surface*) video_buffer;
+}
+
+void nom::SDL_Canvas::setPosition ( const Coords& coords_ )
+{
+  this->coords = coords_;
+}
+
+void nom::SDL_Canvas::setOffsets ( const Coords& offsets_ )
+{
+  this->offsets = offsets_;
+}
+
+const int32_t nom::SDL_Canvas::getCanvasWidth ( void ) const
+{
+  return this->canvas_buffer->w;
+}
+
+const int32_t nom::SDL_Canvas::getCanvasHeight ( void ) const
+{
+  return this->canvas_buffer->h;
+}
+
+void* nom::SDL_Canvas::getCanvasPixelsFormat ( void ) const
+{
+  return this->canvas_buffer->format;
+}
+
+bool nom::SDL_Canvas::loadImageFromFile ( const std::string& filename, const nom::Color& colorkey, uint32_t flags )
+{
+  nom::SDL_Image image;
+  this->canvas_buffer = (SDL_Surface*) image.loadImageFromFile ( filename, colorkey, flags );
+
+  this->offsets.setWidth ( this->canvas_buffer->w );
+  this->offsets.setHeight ( this->canvas_buffer->h );
   return true;
 }
 
-Surface<SDL_Surface> SDL_Canvas::get ( void )
+void nom::SDL_Canvas::Draw ( void* video_buffer )
 {
-  return canvas_buffer;
-}
+  // temporary vars to store our wrapped Coords
+  SDL_Rect blit_coords = this->coords.getSDL_Rect();
+  SDL_Rect blit_offsets = this->offsets.getSDL_Rect();
 
-void SDL_Canvas::Draw ( void* video_buffer ) const
-{
-  // temporary vars to store our wrapped GCoords
-    //SDL_Rect blit_coords = coords.getSDL_Rect();
-    //SDL_Rect blit_offsets = offsets.getSDL_Rect();
-
-  if ( video_buffer == NULL )
+  if ( (SDL_Surface*) video_buffer == NULL )
   {
-    #ifdef DEBUG_GFX
-      std::cout << "ERR in Gfx::DrawSurface(): NULL surface" << std::endl;
+    #ifdef DEBUG_SDL_CANVAS
+      std::cout << "ERR in SDL_Canvas::Draw(): NULL surface" << std::endl;
     #endif
-    return;
-    //return false;
   }
 
-  // Coords ( -1, -1, -1, -1 ) -- our default, is equivalent to NULL for SDL_BlitSurface it seems
-  if ( SDL_BlitSurface ( canvas_buffer.raw_ptr(), NULL, (SDL_Surface*) video_buffer, NULL ) != 0 )
-    //if ( SDL_BlitSurface ( canvas_buffer.raw_ptr(), &blit_offsets, video_buffer.raw_ptr(), &blit_coords ) != 0 )
+  if ( SDL_BlitSurface ( canvas_buffer, &blit_offsets, (SDL_Surface*) video_buffer, &blit_coords ) != 0 )
   {
-    #ifdef DEBUG_GFX
-      std::cout << "ERR in Gfx::DrawSurface() at SDL_BlitSurface(): " << SDL_GetError() << std::endl;
+    #ifdef DEBUG_SDL_CANVAS
+      std::cout << "ERR in SDL_Canvas::Draw() at SDL_BlitSurface(): " << SDL_GetError() << std::endl;
     #endif
-
-    //return false;
   }
-
-  //return true;
 }
+
--- src/SDL_Canvas.hpp
+++ src/SDL_Canvas.hpp
@@ -19,93 +19,47 @@
 
 #include "gamelib.h"
 
-//#include "Color.h"
-//#include "Coords.h"
+#include "Color.h"
+#include "Coords.h"
 #include "gfx.h"
 #include "SDL_Display.h"
-
-#include <memory> // for shared_ptr<T>
-
-namespace nom
-{
-  template <typename T>
-  class Surface
-  {
-  public:
-    template <void (D)(T*)> Surface(T* ptr) : p(ptr, D) { }
-    template <typename D> Surface(T* ptr, D d) : p(ptr, d) { }
-
-    Surface(T* ptr) : p(ptr) { }
-    Surface() : p() { }
-    Surface(const Surface<T>& that) : p(that.p) { }
-
-    T* raw_ptr() const { return p.get(); }
-
-    virtual ~Surface() { }
-
-  //protected:
-    std::shared_ptr<T> p;
-  };
-}
+#include "SDL_Image.hpp"
+#include <memory> // for std::shared_ptr
 
 namespace nom
 {
-  class SDL_Canvas: public Surface<SDL_Surface>
+  class SDL_Canvas
   {
     public:
       SDL_Canvas ( void );
       SDL_Canvas ( int32_t width, int32_t height, const Color& colorbit, uint32_t flags = 0 );
+      SDL_Canvas ( void* video_buffer );
       ~SDL_Canvas ( void );
-      SDL_Canvas ( SDL_Surface *surface )
-      : Surface<SDL_Surface>(surface, SDL_FreeSurface)
-      {}
-      SDL_Canvas ( const Surface& video_buffer )
-      : Surface<SDL_Surface>( video_buffer )
 
-      {
-        //canvas_buffer = video_buffer;
-      }
+      void* get ( void );
 
-      //void setPosition ( const Coords& coords );
-      //void setOffsets ( const Coords& offsets );
-      void setCanvas ( const Surface<SDL_Surface>& video_buffer );
+      void setCanvas ( SDL_Surface *video_buffer );
+      void setCanvas ( void* video_buffer );
 
-      bool loadImage ( std::string filename, const Color& colorkey = nom::Color ( -1, -1, -1, -1 ), unsigned int flags = 0 );
+      void setPosition ( const Coords& coords_ );
+      void setOffsets ( const Coords& offsets_ );
 
-      void Draw ( void* video_buffer ) const;
-                  //const nom::Coords &coords, const nom::Coords &offsets
+      const int32_t getCanvasWidth ( void ) const;
+      const int32_t getCanvasHeight ( void ) const;
+      void* getCanvasPixelsFormat ( void ) const;
 
-      Surface<SDL_Surface> get ( void );
+      bool loadImageFromFile  ( const std::string& filename, const nom::Color&
+                                colorkey = nom::Color ( -1, -1, -1, -1 ),
+                                uint32_t flags = SDL_RLEACCEL | SDL_SRCCOLORKEY );
+
+      void Draw ( void* video_buffer );
 
     private:
-      Surface<SDL_Surface> canvas_buffer;
-      //Coords coords;
-      //Coords offsets;
-      //Color color;
+      SDL_Surface *canvas_buffer;
+      nom::Coords coords;
+      nom::Coords offsets;
+      nom::Color colorkey;
     };
-  class SDL_VideoSurface: public SDL_Canvas
-  {
-    public:
-      SDL_VideoSurface ( SDL_Surface *screen )
-      : SDL_Canvas ( screen )
-      {}
-      SDL_VideoSurface ( int width, int height, int bpp, unsigned int flags = 0 )
-      : SDL_Canvas ( SDL_SetVideoMode ( width, height, bpp, flags ) )
-      {
-      }
-
-      SDL_VideoSurface ( const SDL_VideoSurface& video_buffer )
-      : SDL_Canvas ( video_buffer )
-      {
-      }
-      virtual ~SDL_VideoSurface () {}
-      //static Surface<SDL_Surface> get()
-      //{
-        //return SDL_GetVideoSurface();
-      //}
-    private:
-      //
-  };
 }
 
 #endif // NOMLIB_SDL_SURFACE_HEADERS defined

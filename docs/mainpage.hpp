019de003c1e52c2d23e2b2cc0e4374e7fadbc8f1
Jeffrey Carpenter <jeffrey.carp@gmail.com>
2013-06-07 21:34:51 -0500

    Stub doxygen cfg & index page

---
+++ docs/mainpage.hpp
@@ -0,0 +1,9 @@
+/*! \mainpage nomlib Project Documentation Source
+ * \section intro_sec Introduction
+ * Introduction.
+ * \section install_sec Installation
+ * \subsection step1 Step 1: Stub
+ *
+ * Stub
+ */
+

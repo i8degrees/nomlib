23b56c93cd2277cf3bc18ac747ae96603ce3576a
Jeffrey Carpenter <jeffrey.carp@gmail.com>
2013-06-09 06:50:23 -0500

    Removed using namespace nom declaration

--- src/Color.cpp
+++ src/Color.cpp
@@ -9,16 +9,14 @@
 ******************************************************************************/
 #include "Color.h"
 
-using namespace nom;
-
-Color::Color ( void ) : red ( 0 ), green ( 0 ), blue ( 0 ), alpha ( -1 )
+nom::Color::Color ( void ) : red ( 0 ), green ( 0 ), blue ( 0 ), alpha ( -1 )
 {
   #ifdef DEBUG_COLOR_OBJ
     std::cout << "Color::Color (): " << "Hello, world!" << std::endl << std::endl;
   #endif
 }
 
-Color::Color ( int32_t r, int32_t g, int32_t b, int32_t a )
+nom::Color::Color ( int32_t r, int32_t g, int32_t b, int32_t a )
 {
   #ifdef DEBUG_COLOR_OBJ
     std::cout << "Color::Color (): " << "Hello, world!" << std::endl << std::endl;
@@ -30,7 +28,7 @@ Color::Color ( int32_t r, int32_t g, int32_t b, int32_t a )
   this->alpha = a;
 }
 
-Color::~Color ( void )
+nom::Color::~Color ( void )
 {
   #ifdef DEBUG_COLOR_OBJ
     std::cout << "Color::~Color (): " << "Goodbye cruel world!" << std::endl << std::endl;
@@ -39,32 +37,32 @@ Color::~Color ( void )
   // Stub
 }
 
-const nom::Color Color::getColor ( void ) const
+const nom::Color nom::Color::getColor ( void ) const
 {
   return Color ( this->red, this->green, this->blue, this->alpha );
 }
 
-int32_t Color::getRed ( void ) const
+int32_t nom::Color::getRed ( void ) const
 {
   return this->red;
 }
 
-int32_t Color::getGreen ( void ) const
+int32_t nom::Color::getGreen ( void ) const
 {
   return this->green;
 }
 
-int32_t Color::getBlue ( void ) const
+int32_t nom::Color::getBlue ( void ) const
 {
   return this->blue;
 }
 
-int32_t Color::getAlpha ( void ) const
+int32_t nom::Color::getAlpha ( void ) const
 {
   return this->alpha;
 }
 
-void Color::setColor ( int32_t r, int32_t g, int32_t b, int32_t a )
+void nom::Color::setColor ( int32_t r, int32_t g, int32_t b, int32_t a )
 {
   this->red = r;
   this->green = g;
@@ -72,27 +70,27 @@ void Color::setColor ( int32_t r, int32_t g, int32_t b, int32_t a )
   this->alpha = a;
 }
 
-void Color::setRed ( int32_t r )
+void nom::Color::setRed ( int32_t r )
 {
   this->red = r;
 }
 
-void Color::setGreen ( int32_t g )
+void nom::Color::setGreen ( int32_t g )
 {
   this->green = g;
 }
 
-void Color::setBlue ( int32_t b )
+void nom::Color::setBlue ( int32_t b )
 {
   this->blue = b;
 }
 
-void Color::setAlpha ( int32_t a )
+void nom::Color::setAlpha ( int32_t a )
 {
   this->alpha = a;
 }
 
-uint32_t Color::getColorAsInt ( void* pixel_format ) const
+uint32_t nom::Color::getColorAsInt ( void* pixel_format ) const
 {
   if ( this->alpha != -1 )
     return SDL_MapRGBA ( ( SDL_PixelFormat* ) pixel_format, this->red, this->green, this->blue, this->alpha );
@@ -101,7 +99,7 @@ uint32_t Color::getColorAsInt ( void* pixel_format ) const
 }
 
 // SDL color struct compatibility wrapper
-SDL_Color Color::getSDL_Color ( void ) const
+SDL_Color nom::Color::getSDL_Color ( void ) const
 {
   SDL_Color color;
 

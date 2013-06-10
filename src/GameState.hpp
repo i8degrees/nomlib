a7996ee02c8f1709250638964ae35b8815310eb0
Jeffrey Carpenter <jeffrey.carp@gmail.com>
2013-06-09 20:20:09 -0500

    GameState is now in the nom namespace

--- src/GameState.hpp
+++ src/GameState.hpp
@@ -6,6 +6,7 @@
   Copyright (c) 2013 Jeffrey Carpenter
 
   Portions Copyright (c) 2013 Fielding Johnston
+  All rights reserved.
 
 ******************************************************************************/
 #ifndef NOMLIB_GAMESTATE_HEADERS
@@ -17,23 +18,26 @@
 #include "SDL_Input.hpp"
 #include "gamelib.h"
 
-class GameState: public nom::SDL_Input // "has a" relationship
+namespace nom
 {
-  public:
-    virtual ~GameState()
-    {
-      #ifdef DEBUG_GAMESTATE_OBJ
-        std::cout << "GameState::~GameState (): " << "Goodbye cruel world!" << std::endl << std::endl;
-      #endif
-    }
-    virtual void Pause ( void ) = 0;
-    virtual void Resume ( void ) = 0;
+  class GameState: public nom::SDL_Input // "has a" relationship
+  {
+    public:
+      virtual ~GameState()
+      {
+        #ifdef DEBUG_GAMESTATE_OBJ
+          std::cout << "GameState::~GameState (): " << "Goodbye cruel world!" << std::endl << std::endl;
+        #endif
+      }
+      virtual void Pause ( void ) = 0;
+      virtual void Resume ( void ) = 0;
 
-    virtual void Update ( void* ) = 0;
-    virtual void Draw ( void* ) = 0;
+      virtual void Update ( void* ) = 0;
+      virtual void Draw ( void* ) = 0;
 
-  private:
-    // ...
-};
+    private:
+      // ...
+  };
+}
 
 #endif // NOMLIB_GAMESTATE_HEADERS defined

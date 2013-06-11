10a4177fdcb182d8c3ace7f4765f764918c3c32e
Jeffrey Carpenter <jeffrey.carp@gmail.com>
2013-06-10 19:22:57 -0500

    New SDL primitive (rectangle shape)

---
+++ src/SDL_Rectangle.cpp
@@ -0,0 +1,45 @@
+/******************************************************************************
+    SDL_Rectangle.cpp
+
+  SDL Rectangle Primitive
+
+  Copyright (c) 2013 Jeffrey Carpenter
+  All rights reserved.
+
+******************************************************************************/
+#include "SDL_Rectangle.hpp"
+
+nom::Rectangle::Rectangle ( void )
+{
+  this->coords.setCoords ( 0, 0, 0, 0 );
+  this->color.setColor ( 0, 0, 0, SDL_ALPHA_OPAQUE );
+}
+
+nom::Rectangle::~Rectangle ( void )
+{
+  // ...
+}
+
+nom::Rectangle::Rectangle ( const nom::Coords& coords, const nom::Color& color )
+{
+  this->coords = coords;
+  this->color = color;
+}
+
+void nom::Rectangle::Draw ( void* video_buffer )
+{
+  SDL_Rect rectangle = this->coords.getSDL_Rect();
+  unsigned int rectangle_color = 0;
+  SDL_Surface *buffer = (SDL_Surface*) video_buffer;
+
+  rectangle_color = this->color.getColorAsInt ( buffer->format );
+
+  if ( SDL_FillRect ( buffer, &rectangle, rectangle_color ) != 0 )
+  {
+    std::cout << "ERR in nom::Rectangle::Draw(): " << SDL_GetError() << std::endl;
+
+    return;
+  }
+
+  return;
+}
---
+++ src/SDL_Rectangle.hpp
@@ -0,0 +1,41 @@
+/******************************************************************************
+    SDL_Rectangle.hpp
+
+  SDL Rectangle Primitive
+
+  Copyright (c) 2013 Jeffrey Carpenter
+  All rights reserved.
+
+******************************************************************************/
+#ifndef NOMLIB_SDL_RECTANGLE_HEADERS
+#define NOMLIB_SDL_RECTANGLE_HEADERS
+
+#include <iostream>
+#include <string>
+
+#include "SDL.h"
+
+#include "Coords.h"
+#include "Color.h"
+#include "SDL_Drawable.hpp"
+#include "Transformable.hpp"
+#include "gamelib.h"
+
+namespace nom
+{
+  class Rectangle:  public nom::SDL_Drawable,   // "is a" relationship
+                    public nom::Transformable   // "has a" relationship
+
+  {
+    public:
+      Rectangle ( void );
+      virtual ~Rectangle ( void );
+      Rectangle ( const nom::Coords& coords, const nom::Color& color );
+
+      void Draw ( void* );
+    private:
+      //
+  };
+}
+
+#endif // NOMLIB_SDL_RECTANGLE_HEADERS defined

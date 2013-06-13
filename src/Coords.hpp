99b0d3174612e076346e6963a6e63c704f9aa296
Jeffrey Carpenter <jeffrey.carp@gmail.com>
2013-06-12 22:22:09 -0500

    Added comparison operators for nom::Coords objects

--- src/Coords.hpp
+++ src/Coords.hpp
@@ -60,19 +60,90 @@ namespace nom
 
       void updateCoords ( signed int x_ = 0, signed int y_ = 0, signed int width_ = 0, signed int height_ = 0 );
 
-      //signed int x;
-      //signed int y;
-      //signed int z;
-      //signed int width;
-      //signed int height;
-
-    private:
       signed int x;
       signed int y;
       signed int z; // reserved
       signed int width;
       signed int height;
+
+    private:
+      // ...
   };
+
+  // FIXME: not sure why these cannot be put into the class file without
+  // linking errs?
+  inline bool operator == ( const nom::Coords& left, const nom::Coords& right )
+  {
+    return (left.x == right.x ) &&
+           (left.y == right.y ) &&
+           (left.width == right.width ) &&
+           (left.height == right.height );
+  }
+
+  inline bool operator != ( const nom::Coords& left, const nom::Coords& right )
+  {
+    return ! ( left == right );
+  }
+
+  inline nom::Coords operator + ( const nom::Coords& left, const nom::Coords& right )
+  {
+    return nom::Coords  ( static_cast<int32_t> ( left.x + right.x ),
+                          static_cast<int32_t> ( left.y + right.y ),
+                          static_cast<int32_t> ( left.width + right.width ),
+                          static_cast<int32_t> ( left.height + right.height )
+                        );
+  }
+
+  inline nom::Coords operator ++ ( nom::Coords& left )
+  {
+    return nom::Coords  ( static_cast<int32_t> ( left.x ++ ),
+                          static_cast<int32_t> ( left.y ++ ),
+                          static_cast<int32_t> ( left.width ++ ),
+                          static_cast<int32_t> ( left.height ++ )
+                        );
+  }
+
+  inline nom::Coords operator - ( const nom::Coords& left, const nom::Coords& right )
+  {
+    return nom::Coords  ( static_cast<int32_t> ( left.x - right.x ),
+                          static_cast<int32_t> ( left.y - right.y ),
+                          static_cast<int32_t> ( left.width - right.width ),
+                          static_cast<int32_t> ( left.height - right.height )
+                        );
+  }
+
+  inline nom::Coords operator -- ( nom::Coords& left )
+  {
+    return nom::Coords  ( static_cast<int32_t> ( left.x -- ),
+                          static_cast<int32_t> ( left.y -- ),
+                          static_cast<int32_t> ( left.width -- ),
+                          static_cast<int32_t> ( left.height -- )
+                        );
+  }
+
+  inline nom::Coords operator * ( const nom::Coords& left, const nom::Coords& right)
+  {
+    return nom::Coords  ( static_cast<int32_t> ( left.x * right.x ),
+                          static_cast<int32_t> ( left.y * right.y ),
+                          static_cast<int32_t> ( left.width * right.width ),
+                          static_cast<int32_t> ( left.height * right.height )
+                        );
+  }
+
+  inline nom::Coords& operator += ( nom::Coords& left, const nom::Coords& right)
+  {
+    return left = left + right;
+  }
+
+  inline nom::Coords& operator -= ( nom::Coords& left, const nom::Coords& right )
+  {
+    return left = left - right;
+  }
+
+  inline nom::Coords& operator *= ( nom::Coords& left, const nom::Coords& right)
+  {
+    return left = left * right;
+  }
 }
 
 #endif // NOMLIB_COORDS_HEADERS defined

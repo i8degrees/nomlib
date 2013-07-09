bc7f3025639503b2ccb16769955d6b350f755ffc
Jeffrey Carpenter <jeffrey.carp@gmail.com>
2013-07-09 01:08:24 -0500

    Add 64-bit type definition; add missing assert for bit, ushort, ulong

--- src/nomlib_types.hpp
+++ src/nomlib_types.hpp
@@ -11,6 +11,9 @@
 
 #include <cstdint>
 
+// 64-bit kernel
+#define NOMLIB_64BIT_TYPE
+
 // Derives from stdint.h
 namespace nom {
 
@@ -30,12 +33,14 @@ typedef int32_t int32;
 typedef uint32_t uint32;
 
 // 64-bit integer types
-#if defined ( _MSC_VER ) // MSVC++
-  typedef signed __int64 int64;
-  typedef unsigned __int64 uint64;
-#else
-  typedef int64_t int64;
-  typedef uint64_t uint64;
+#if defined NOMLIB_64BIT_TYPE
+  #if defined ( _MSC_VER ) // MSVC++
+    typedef signed __int64 int64;
+    typedef unsigned __int64 uint64;
+  #else
+    typedef int64_t int64;
+    typedef uint64_t uint64;
+  #endif
 #endif
 
 // Additional integer type definitions
@@ -44,17 +49,26 @@ typedef unsigned long ulong;
 
 } // namespace nom
 
+// Thanks to SDL for the macro! :-)
 // Ensure our defined data types have the right sizes
 #define NOMLIB_COMPILE_TIME_ASSERT(name, x) \
   typedef int NOMLIB_dummy_ ## name[(x) * 2 - 1]
 
+NOMLIB_COMPILE_TIME_ASSERT(bit, sizeof(nom::bit) == 1);
 NOMLIB_COMPILE_TIME_ASSERT(uint8, sizeof(nom::uint8) == 1);
 NOMLIB_COMPILE_TIME_ASSERT(int8, sizeof(nom::int8) == 1);
 NOMLIB_COMPILE_TIME_ASSERT(uint16, sizeof(nom::uint16) == 2);
 NOMLIB_COMPILE_TIME_ASSERT(int16, sizeof(nom::int16) == 2);
 NOMLIB_COMPILE_TIME_ASSERT(uint32, sizeof(nom::uint32) == 4);
 NOMLIB_COMPILE_TIME_ASSERT(int32, sizeof(nom::int32) == 4);
-NOMLIB_COMPILE_TIME_ASSERT(uint64, sizeof(nom::uint64) == 8);
-NOMLIB_COMPILE_TIME_ASSERT(int64, sizeof(nom::int64) == 8);
+NOMLIB_COMPILE_TIME_ASSERT(ushort, sizeof(nom::ushort) == 2);
+
+#if defined NOMLIB_64BIT_TYPE
+  NOMLIB_COMPILE_TIME_ASSERT(uint64, sizeof(nom::uint64) == 8);
+  NOMLIB_COMPILE_TIME_ASSERT(int64, sizeof(nom::int64) == 8);
+  NOMLIB_COMPILE_TIME_ASSERT(ulong, sizeof(nom::ulong) == 8);
+#else
+  NOMLIB_COMPILE_TIME_ASSERT(ulong, sizeof(nom::ulong) == 4);
+#endif
 
 #endif // NOMLIB_TYPES_HEADERS defined

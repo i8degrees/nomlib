4af90f5e46062e69af6d3fbb06031b7b367b9f51
Jeffrey Carpenter <jeffrey.carp@gmail.com>
2013-07-09 11:47:23 -0500

    Add methods getBaseName, getResourcePath & use c++ string instead chars
    	We also add doxygen documentation as well as method documentation

--- src/sys/OSXFS.cpp
+++ src/sys/OSXFS.cpp
@@ -17,10 +17,30 @@ nom::OSXFS::~OSXFS ( void )
 NOMLIB_LOG_INFO;
 }
 
-// Not sure how else to do this but in straight C
-const char* nom::OSXFS::getDirName ( char* path )
+const std::string nom::OSXFS::getBaseName ( const std::string& dir_path )
 {
-  // ERR checking
+  // We must do this string conversion -- from a std::string to a char
+  // pointer -- because the c_str method returns a const char pointer, which
+  // cannot be used for the dirname function, as this call modifies the contents
+  // of the passed variable.
+
+  char path[PATH_MAX];
+
+  std::strcpy ( path, dir_path.c_str() );
+
+  return basename ( path );
+}
+
+const std::string nom::OSXFS::getDirName ( const std::string& dir_path )
+{
+  // We must do this string conversion -- from a std::string to a char
+  // pointer -- because the c_str method returns a const char pointer, which
+  // cannot be used for the dirname function, as this call modifies the contents
+  // of the passed variable.
+
+  char path[PATH_MAX];
+
+  std::strcpy ( path, dir_path.c_str() );
 
   return dirname ( path );
 }
@@ -31,16 +51,32 @@ const std::string nom::OSXFS::getWorkingDir ( void )
 
   getcwd ( path, PATH_MAX );
 
-  // ERR checking
-
   std::string cwd ( path );
 
   return cwd;
 }
 
-void nom::OSXFS::setWorkingDir ( std::string path )
+void nom::OSXFS::setWorkingDir ( const std::string& path )
 {
   chdir ( path.c_str() );
+}
 
-  // ERR checking
+const std::string nom::OSXFS::getResourcesPath ( void )
+{
+  char resources_path[ PATH_MAX ];
+  CFBundleRef mainBundle = CFBundleGetMainBundle();
+  CFURLRef resourcesURL = CFBundleCopyResourcesDirectoryURL ( mainBundle );
+
+  if ( ! CFURLGetFileSystemRepresentation ( resourcesURL, true, ( uint8* ) resources_path, PATH_MAX ) )
+  {
+NOMLIB_LOG_ERR ( "Could not obtain the bundle's Resources path." );
+
+    CFRelease ( resourcesURL );
+
+    return getWorkingDir();
+  }
+
+  CFRelease ( resourcesURL );
+
+  return resources_path;
 }
--- src/sys/OSXFS.hpp
+++ src/sys/OSXFS.hpp
@@ -12,28 +12,53 @@
 #include <string>
 #include <unistd.h>
 #include <libgen.h>
+#include <CoreFoundation/CoreFoundation.h>
 
 #include "config.hpp"
 
 #ifndef PATH_MAX
-  #define PATH_MAX 1024 // TODO: research this further, no idea if 1024 is legit
+  #define PATH_MAX 1024
 #endif
 
-namespace nom
+namespace nom {
+
+/// \brief Platform-agnostic file system interface
+class OSXFS
 {
-  class OSXFS
-  {
-    public:
-      OSXFS ( void );
-      ~OSXFS ( void );
+  public:
+    ~OSXFS ( void );
 
-      static const char* getDirName ( char* path ); // dirname(3) wrapper
-      static const std::string getWorkingDir ( void ); // getcwd(3) wrapper
-      static void setWorkingDir ( std::string path ); // chdir(2) wrapper
+    /// basename(3) wrapper
+    ///
+    /// dir_path is arbitrarily limited to 1024 characters.
+    static const std::string getBaseName ( const std::string& dir_path );
 
-    private:
-      // ...
-  };
-}
+    /// dirname(3) wrapper
+    ///
+    /// dir_path is arbitrarily limited to 1024 characters.
+    static const std::string getDirName ( const std::string& dir_path );
+
+    /// getcwd(3) wrapper
+    ///
+    /// Returned path is arbitrarily limited to 1024 characters.
+    static const std::string getWorkingDir ( void );
+
+    /// chdir(2) wrapper
+    static void setWorkingDir ( const std::string& path );
+
+    /// Returns the path to the Resources folder (OSX Application Bundle)
+    /// \todo TEST ME
+    static const std::string getResourcesPath ( void );
+
+  private:
+    OSXFS ( void );
+};
+
+
+} // namespace nom
 
 #endif // NOMLIB_OSXFS_HEADERS defined
+
+/// \class nom::OSXFS
+///
+/// \todo ERR checking & research PATH_MAX macro #define

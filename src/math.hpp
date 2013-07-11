d2752a7556fc05f4e72a730973842c0768663982
Jeffrey Carpenter <jeffrey.carp@gmail.com>
2013-07-11 03:44:20 -0500

    Introduce Vector2, Vector3, Vector4 template classes

--- src/math.hpp
+++ src/math.hpp
@@ -13,6 +13,9 @@
 
 #include "math/Color.hpp"
 #include "math/Coords.hpp"
+#include "math/Vector2-inl.hpp"
+#include "math/Vector3-inl.hpp"
+#include "math/Vector4-inl.hpp"
 #include "math/Transformable.hpp"
 
 #endif // NOMLIB_MATH_HEADERS defined

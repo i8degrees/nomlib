ca8b766333492f7c425e51fc3687e55f0f72805b
Jeffrey Carpenter <jeffrey.carp@gmail.com>
2013-07-17 15:45:37 -0500

    Introduce Template class & header files

---
+++ Template.cpp
@@ -0,0 +1,28 @@
+/******************************************************************************
+
+    C++ Template Class; don't forget to add me to CMakeLists.txt
+
+  Copyright (c) 2013 Jeffrey Carpenter
+  All rights reserved.
+
+******************************************************************************/
+#include "Template.hpp"
+
+namespace nom {
+
+Template::Template ( void )
+{
+NOMLIB_LOG_INFO;
+
+  // Initialize instance variables
+}
+
+Template::~Template ( void )
+{
+NOMLIB_LOG_INFO;
+
+  // Clean up instance variables
+}
+
+
+} // namespace nom
---
+++ Template.hpp
@@ -0,0 +1,32 @@
+/******************************************************************************
+
+    C++ Template Header; don't forget to add me to CMakeLists.txt
+
+  Copyright (c) 2013 Jeffrey Carpenter
+  All rights reserved.
+
+******************************************************************************/
+#ifndef NOMLIB_TEMPLATE_HPP
+#define NOMLIB_TEMPLATE_HPP
+
+#include <iostream>
+#include <string>
+
+#include "nomlib/config.hpp"
+#include "nomlib/sys/Logger.hpp"
+
+namespace nom {
+
+class Template
+{
+  public:
+    Template ( void );
+    ~Template ( void );
+
+  private:
+    // ...
+};
+
+} // namespace nom
+
+#endif // include guard defined

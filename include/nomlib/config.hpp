fb3ee13c52b67960853b1abf62e1efc5d3485b41
Jeffrey Carpenter <jeffrey.carp@gmail.com>
2013-08-01 05:18:50 -0500

    Introduce DEBUG, DEBUG_TRACE, DEBUG_ASSERT options
    	* BUILD_EXAMPLES & BUILD_DOCS options off by default
    	* Remove BUILD_RELEASE option (if not DEBUG, we assume release)
    	* Rename NOMLIB_BUILD_EXAMPLES to BUILD_EXAMPLES

--- include/nomlib/config.hpp
+++ include/nomlib/config.hpp
@@ -17,14 +17,28 @@
 // Platform detection
 #include "nomlib/platforms.hpp"
 
-// nomlib debugging
-
-// Standard debug level; logging of info, warnings & errors
-#define NOM_DEBUG
-
-// Internal development; logging of class object construction and destruction
-#define NOM_DEBUG_TRACE
-
+/// \brief nomlib Debugging Options
+///
+/// Available preprocessor flags:
+///
+///     DEBUG
+/// Activates output of all info, warning and error messages.
+///
+///     DEBUG_ASSERT
+/// Activate run-time assert checks scattered throughout the code-base.
+///
+///     DEBUG_TRACE
+/// Special debug level for activating output of each class object construction
+/// and destruction that occurs. Unless you are an active developer of nomlib,
+/// or perhaps suspect that an error in your software could be caused by nomlib,
+/// you are unlikely to be interested in this debug level -- this option
+/// generates a hefty amount of output text!
+///
+/// These definitions are passed at build time for activation like so:
+///
+///     cd build && cmake -DDEBUG=on ..
+///
+/// You may mix and match these flags however you wish.
 
 #ifndef __cplusplus
   #warning "nomlib requires a C++11 compiler"

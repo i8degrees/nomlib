512a3d6667ec36931b366ceca9923f50de310357
Jeffrey Carpenter <jeffrey.carp@gmail.com>
2013-08-09 22:45:50 -0500

    Remove broken version of sndfile.framework and replace with SFMLv2 ver
    	* This is (hopefully) a temporary workaround until I bother with
    getting a new sndfile framework bundled. The previous one we built had
    no FLAC or OGG support built in -_-

--- extlibs/osx/src/libsndfile/download.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-LIBSNDFILE=libsndfile-1.0.25
-LIBSNDFILE_TAR=${LIBSNDFILE}.tar.gz
-
-wget http://www.mega-nerd.com/libsndfile/files/${LIBSNDFILE_TAR}
-tar xzvf ${LIBSNDFILE_TAR}
-mv -v ${LIBSNDFILE} libsndfile-src
-rm -fv libsndfile-1.0.25.tar.gz
-

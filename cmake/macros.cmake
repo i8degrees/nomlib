2b3f12bc9cf0e57ebe05f66b3e8d764bb6c6d992
Jeffrey Carpenter <jeffrey.carp@gmail.com>
2013-08-13 12:28:09 -0500

    Introduce change_rpath, install_name_rpath & patch sndfile rpaths

--- cmake/macros.cmake
+++ cmake/macros.cmake
@@ -14,3 +14,25 @@ macro ( add_rpath rpath binary_path )
   endforeach ( path ${rpath} )
 
 endmacro ( add_rpath rpath binary_path )
+
+# Modify runtime search path for a library or application
+macro ( change_rpath old_rpath new_rpath binary_path )
+
+  add_custom_command  ( TARGET ${PROJECT_NAME}
+                        COMMAND ${CMAKE_INSTALL_NAME_TOOL}
+                        -change "${old_rpath}" "${new_rpath}" "${binary_path}"
+                        COMMENT "\nModifying runtime search path for ${binary_path}: \n\n\t${old_rpath}\n\nto ${new_rpath}\n\n"
+                      )
+
+endmacro ( change_rpath rpath binary_path )
+
+# Change the install name path of a library
+macro ( install_name_rpath rpath binary_path )
+
+  add_custom_command  ( TARGET ${PROJECT_NAME}
+                        COMMAND ${CMAKE_INSTALL_NAME_TOOL}
+                        -id "${rpath}" "${binary_path}"
+                        COMMENT "\nModifying install name path for ${binary_path}: \n\n\t${rpath}\n\n"
+                      )
+
+endmacro ( install_name_rpath rpath binary_path )

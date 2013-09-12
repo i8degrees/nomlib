13f86083307c2afacec8349e320afb27bde7010d
Jeffrey Carpenter <jeffrey.carp@gmail.com>
2013-09-11 20:26:56 -0500

    Introduce AnimatedSprite class

--- include/nomlib/graphics.hpp
+++ include/nomlib/graphics.hpp
@@ -46,6 +46,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <nomlib/graphics/Rectangle.hpp>
 #include <nomlib/graphics/Sprite.hpp>
 #include <nomlib/graphics/SpriteSheet.hpp>
+#include <nomlib/graphics/AnimatedSprite.hpp>
 
 
 #endif // include guard defined

16d4b54c95fdc7ff7fb271613f0b6c5987937fed
Jeffrey Carpenter <jeffrey.carp@gmail.com>
2013-09-28 11:24:28 -0500

    Introduce nomlib Unit Tests

---
+++ tests/system.cpp
@@ -0,0 +1,106 @@
+/******************************************************************************
+
+  nomlib - C++11 cross-platform game engine
+
+Copyright (c) 2013, Jeffrey Carpenter <jeffrey.carp@gmail.com>
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+******************************************************************************/
+#include <iostream>
+#include <cstdlib>
+#include <cassert>
+
+#include <nomlib/system.hpp>
+
+/// nom::File Unit Test 1
+nom::int32 do_file_test1 ( nom::int32 args_count, char* args[] );
+
+/// nom::File Unit Test 2
+nom::int32 do_file_test2 ( void );
+
+/// nom::File Unit Test 3
+nom::int32 do_file_test3 ( void );
+
+/// nom::File Unit Test 4
+nom::int32 do_file_test4 ( void );
+
+nom::int32 do_file_test1 ( nom::int32 args_count, char* args[] )
+{
+  nom::File dir;
+  std::string pwd = "\0";
+
+  pwd = dir.path ( args[0] );
+
+  if ( args_count < 1 ) return EXIT_FAILURE;
+  if ( pwd == "" || pwd == "\0" ) return EXIT_FAILURE;
+
+  return EXIT_SUCCESS;
+}
+
+nom::int32 do_file_test2 ( void )
+{
+  nom::Path p;
+
+  #if defined ( NOM_PLATFORM_OSX ) || defined ( NOM_PLATFORM_LINUX ) || defined ( NOM_PLATFORM_POSIX )
+    if ( p.native().compare ("/") != 0 ) return EXIT_FAILURE;
+  #elif defined ( NOM_PLATFORM_WINDOWS )
+    if ( p.native().compare ("\\") != 0 ) return EXIT_FAILURE;
+  #else // Unsupported
+    return EXIT_FAILURE;
+  #endif
+
+  return EXIT_SUCCESS;
+}
+
+nom::int32 do_file_test3 ( void ) { return EXIT_SUCCESS; }
+nom::int32 do_file_test4 ( void ) { return EXIT_SUCCESS; }
+
+nom::int32 main ( nom::int32 args_count, char* args[] )
+{
+  if ( do_file_test1 ( args_count, args ) != EXIT_SUCCESS )
+  {
+    NOM_LOG_ERR ( NOM_UNIT_TEST, "Failed nom::File unit test 1" );
+    return EXIT_FAILURE;
+  }
+
+  if ( do_file_test2() != EXIT_SUCCESS )
+  {
+    NOM_LOG_ERR ( NOM_UNIT_TEST, "Failed nom::File unit test 2" );
+    return EXIT_FAILURE;
+  }
+
+  if ( do_file_test3() != EXIT_SUCCESS )
+  {
+    NOM_LOG_ERR ( NOM_UNIT_TEST, "Failed nom::File unit test 3" );
+    return EXIT_FAILURE;
+  }
+
+  if ( do_file_test4() != EXIT_SUCCESS )
+  {
+    NOM_LOG_ERR ( NOM_UNIT_TEST, "Failed nom::File unit test 4" );
+    return EXIT_FAILURE;
+  }
+
+  NOM_LOG_INFO ( NOM_UNIT_TEST, "System Unit Tests: Pass!" );
+  return EXIT_SUCCESS;
+}

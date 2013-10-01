8feacb2881e7bd79fc508e3c5d8e9e3380f38f24
Ivan Rubinson <soryy708@gmail.com>
2013-10-01 04:02:30 +0200

    Update file contents to renamed header

--- src/system/IDialogMessageBox.cpp
+++ src/system/IDialogMessageBox.cpp
@@ -26,7 +26,7 @@ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 ******************************************************************************/
-#include "nomlib/system/DialogMessageBox.hpp"
+#include "nomlib/system/IDialogMessageBox.hpp"
 
 namespace nom {
 

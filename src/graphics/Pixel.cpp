b2ec308bbf93b0ab21d9d79418d81f3457d66914
Jeffrey Carpenter <jeffrey.carp@gmail.com>
2013-10-05 11:39:21 -0500

    Fix file paths of Pixel & Point classes
    	* My intentions were to have Pixel in graphics and Point in shapes;
    thanks to Ivan & MSVC++ complaining, I caught this mistake much earlier
    than I would have otherwise :)

--- src/graphics/Pixel.cpp
+++ src/graphics/Pixel.cpp
@@ -26,7 +26,7 @@ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 ******************************************************************************/
-#include "nomlib/graphics/shapes/Pixel.hpp"
+#include "nomlib/graphics/Pixel.hpp"
 
 namespace nom {
 

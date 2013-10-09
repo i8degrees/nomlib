2b384bc8b01144b0bb84c76436bc4a4534cf79e4
Ivan Rubinson <soryy708@gmail.com>
2013-10-10 00:36:45 +0200

    Uncomment useful thing in windev.bat

--- windev.bat
+++ windev.bat
@@ -1,6 +1,6 @@
 :: SOURCE: http://573.github.io/blog/2010/10/13/adopt-a-visual-c-plus-plus-express-edition-project-with-cmake/
 ::
-::@echo off
+@echo off
 
 :: Setup the development environment for CMake -- we need a compiler and linker.
 @set PATH=C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\bin;%PATH%

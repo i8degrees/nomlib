1437ffd14f7c4152a0baa3a513a3aa2b79248806
Jeffrey Carpenter <jeffrey.carp@gmail.com>
2013-12-11 22:31:16 -0600

    Move platform specific cfg to platform.cmake

--- cmake/platform.cmake
+++ cmake/platform.cmake
@@ -6,10 +6,21 @@ if ( CMAKE_SYSTEM_NAME STREQUAL "Darwin" )
   option ( FRAMEWORK "Build OSX Framework instead of dylib" on )
   option ( UNIVERSAL "Build as an OSX Universal Library" off )
 
-  message ( STATUS "Platform: Darwin (OS X)" )
-elseif ( CMAKE_SYSTEM_NAME STREQUAL "Linux" )
+  # libc++ requires OSX v10.7+
+  set ( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11 -stdlib=libc++" )
 
+  if ( CMAKE_GENERATOR STREQUAL "Xcode" )
+    set ( CMAKE_XCODE_ATTRIBUTE_CLANG_CXX_LANGUAGE_STANDARD "c++11" )
+    set ( CMAKE_XCODE_ATTRIBUTE_CLANG_CXX_LIBRARY "libc++" )
+  endif ( CMAKE_GENERATOR STREQUAL "Xcode" )
+
+  message ( STATUS "Platform: Darwin (Mac OS X)" )
+elseif ( CMAKE_SYSTEM_NAME STREQUAL "Linux" ) # Tested on Ubuntu v12.04-LTS
   set ( PLATFORM_LINUX true )
+
+  # Clang is not supported on Linux due to libc++ not being distributed by
+  # default yet
+  set ( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++0x" )
 
   # ARCH_32 and ARCH_64 are not presently used here, but are reserved for future
   # consistency with the other supported platforms.
@@ -20,16 +31,21 @@ elseif ( CMAKE_SYSTEM_NAME STREQUAL "Linux" )
 elseif ( CMAKE_SYSTEM_NAME STREQUAL "Windows" )
   set ( PLATFORM_WINDOWS true )
 
+  set ( CMAKE_CONFIGURATION_TYPES "${CMAKE_BUILD_TYPE}" )
+
   option ( ARCH_32 "Compile ${PROJECT_NAME} as a 32-bit library" off )
   option ( ARCH_64 "Compile ${PROJECT_NAME} as a 64-bit library" on )
 
   message ( STATUS "Platform: Windows" )
 else () # Not Linux nor OSX
-
   set ( PLATFORM_UNKNOWN true )
-  message ( STATUS "Platform: Unknown" )
 
+  # Use whatever CMake gathers for us and pray for the best!
+
+  message ( STATUS "Platform: Unknown" )
 endif ( CMAKE_SYSTEM_NAME STREQUAL "Darwin" )
+
+message ( STATUS "Generating build files for: ${CMAKE_GENERATOR}" )
 
 # I was able to shave off close to 50% time compiling nomlib by leaving
 # the UNIVERSAL option off by default.
@@ -44,9 +60,9 @@ if ( UNIVERSAL )
   set ( PLATFORM_ARCH "x86; x64" ) # Reserved for future use
 
 else ( NOT UNIVERSAL )
+  # TODO: We should probably consider bumping the architecture down to i386 $(ARCHS_STANDARD_32_BIT)
   set ( CMAKE_OSX_ARCHITECTURES x86_64 )
   set ( PLATFORM_ARCH "x64" ) # Reserved for future use
-
 endif ( UNIVERSAL )
 
 if ( PLATFORM_WINDOWS AND ARCH_32 )

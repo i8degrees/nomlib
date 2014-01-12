f323828cfce3bfbe3adebd33a6fd8df289bdc1a3
Jeffrey Carpenter <jeffrey.carp@gmail.com>
2014-01-12 09:40:00 -0600

    tests/Makefile: Compile with NOM_DEBUG_TRACE
    tests/run.sh: Do not run tests unless make was successful

--- tests/run.sh
+++ tests/run.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
-make -B test
-./system
-./math
-./graphics
 
+# Full path to make executable
+MAKE_BIN=$(which make)
+
+# Unconditionally make all targets while staying quiet about it
+MAKE_PARAMS="-B --quiet"
+
+$MAKE_BIN $MAKE_PARAMS test # target
+
+if [[ $? -eq 0 ]]; then
+    ./system
+    ./math
+    ./graphics
+fi

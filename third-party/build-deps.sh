bb60f4429da4c63bc0955d92551b7ff91d174b15
Jeffrey Carpenter <jeffrey.carp@gmail.com>
2014-04-02 02:34:13 -0500

    third-party/build-deps.sh: Bit of clean up
    * Introduce timestamping of archive files
    * Add ios, linux platforms for archiving deps

--- third-party/build-deps.sh
+++ third-party/build-deps.sh
@@ -24,23 +24,73 @@ TAR_ARGS="-czvf"
 ZIP_BIN=$(which zip)
 ZIP_ARGS="-r"
 
+# Exclusion masks for zip & tar
 EXCLUSION_MASKS="*.DS_Store*"
 
-if [[ $1 == "osx" ]]; then
-  DEPS_FILENAME="./nomlib_osx-dependencies.tar.gz"
-  INCLUSION_MASKS="osx/"
+PROJECT_NAME="nomlib"
+
+# Date binary for prefixing the created timestamp onto resulting archive
+DATE_BIN=$(which date)
+
+TIMESTAMP="$($DATE_BIN +%Y-%m-%d)" # BSD date(1)
+
+function usage_info()
+{
+  echo "Usage: ./$0 [osx|ios|linux|windows|all]"
+}
+
+function osx_deps()
+{
+  DEPS_FILENAME="./${TIMESTAMP}_${PROJECT_NAME}_osx-dependencies.tar.gz"
+  INCLUSION_MASKS="osx/ common/"
 
   ${TAR_BIN} ${TAR_ARGS} ${DEPS_FILENAME} --exclude=${EXCLUSION_MASKS} ${INCLUSION_MASKS}
-elif [[ $1 == "windows" ]]; then
-  DEPS_FILENAME="./nomlib_windows-dependencies.zip"
-  INCLUSION_MASKS="windows/"
+}
+
+function ios_deps()
+{
+  DEPS_FILENAME="./${TIMESTAMP}_${PROJECT_NAME}_ios-dependencies.tar.gz"
+  INCLUSION_MASKS="ios/ common/"
+
+  ${TAR_BIN} ${TAR_ARGS} ${DEPS_FILENAME} --exclude=${EXCLUSION_MASKS} ${INCLUSION_MASKS}
+}
+
+function windows_deps()
+{
+  DEPS_FILENAME="./${TIMESTAMP}_${PROJECT_NAME}_windows-dependencies.zip"
+  INCLUSION_MASKS="windows/ common/"
 
   ${ZIP_BIN} ${ZIP_ARGS} ${DEPS_FILENAME} ${INCLUSION_MASKS} -x ${EXCLUSION_MASKS}
-#elif [[ $1 == "linux" ]]; then
-  # TODO: Handle Linux platform case
-#elif [[ $1 == "ios" ]]; then
-  # TODO: Handle iOS platform case
+}
+
+function linux_deps()
+{
+  echo "TODO"
+}
+
+function all_deps()
+{
+  osx_deps
+  ios_deps
+  linux_deps
+  windows_deps
+}
+
+if [[ "$1" == "osx" ]]; then
+  osx_deps
+elif [[ $1 == "ios" ]]; then
+  ios_deps
+elif [[ $1 == "linux" ]]; then
+  linux_deps
+elif [[ $1 == "windows" ]]; then
+  windows_deps
+elif [[ $1 == "ios" ]]; then
+  ios_deps
+elif [[ $1 == "all" ]]; then
+  all_deps
+elif [[ $1 == "?" ]]; then
+  usage_info
 else
-  echo "Usage: ./build-deps.sh [osx|windows]"
-  #echo "Usage: ./build-deps.sh [osx|windows|linux|ios]"
+  usage_info
 fi
+

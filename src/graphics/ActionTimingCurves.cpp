febd033539d5a8677367b8448da6f6e3888a85d3
Jeffrey Carpenter <i8degrees@gmail.com>
2015-02-28 20:33:50 -0600

    [actions] ActionTimingCurves: Update doc comments
    * ActionTimingCurves.cpp: Add copyright notice for easing algorithms.
    * ActionTimingCurves.hpp: Update documentation comments.

--- src/graphics/ActionTimingCurves.cpp
+++ src/graphics/ActionTimingCurves.cpp
@@ -25,6 +25,8 @@ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+Easing algorithms are Copyright (c) 2001 Robert Penner
+
 ******************************************************************************/
 #include "nomlib/graphics/ActionTimingCurves.hpp"
 

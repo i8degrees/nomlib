7d440114194fd7b87a7fe192b3ee219de81b06cd
Jeffrey Carpenter <i8degrees@gmail.com>
2015-03-18 22:48:32 -0500

    Fix a few memory leaks observed from TTcards
    * SpriteSheet: Forgot to free the deserializer in ::load_file.
    
    * RocketSDL2RenderInterface: ctx_ is a non-owned pointer, thus we
    do not need to allocate it.
    
    * RocketSDL2RenderInterface: Calling nom::Texture's destructor is
    necessary to free all associated texture memory.

--- include/nomlib/gui/RocketSDL2RenderInterface.hpp
+++ include/nomlib/gui/RocketSDL2RenderInterface.hpp
@@ -89,7 +89,11 @@ class RocketSDL2RenderInterface: public Rocket::Core::RenderInterface
     virtual void Release();
 
     /// Called by Rocket when it wants to render geometry that it does not wish to optimise.
-    virtual void RenderGeometry(Rocket::Core::Vertex* vertices, int num_vertices, int* indices, int num_indices, Rocket::Core::TextureHandle texture, const Rocket::Core::Vector2f& translation);
+    virtual void
+    RenderGeometry( Rocket::Core::Vertex* vertices, int num_vertices,
+                    int* indices, int num_indices,
+                    Rocket::Core::TextureHandle texture_handle,
+                    const Rocket::Core::Vector2f& translation);
 
     /// Called by Rocket when it wants to enable or disable scissoring to clip content.
     virtual void EnableScissorRegion(bool enable);
@@ -105,8 +109,10 @@ class RocketSDL2RenderInterface: public Rocket::Core::RenderInterface
     virtual bool LoadTexture(Rocket::Core::TextureHandle& texture_handle, Rocket::Core::Vector2i& texture_dimensions, const Rocket::Core::String& source);
     /// Called by Rocket when a texture is required to be built from an internally-generated sequence of pixels.
     virtual bool GenerateTexture(Rocket::Core::TextureHandle& texture_handle, const Rocket::Core::byte* source, const Rocket::Core::Vector2i& source_dimensions);
+
     /// Called by Rocket when a loaded texture is no longer required.
-    virtual void ReleaseTexture(Rocket::Core::TextureHandle texture_handle);
+    virtual void
+    ReleaseTexture(Rocket::Core::TextureHandle texture_handle);
 
     /// \brief nomlib interface bridge between SDL2 and libRocket
     ///

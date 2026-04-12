69a9364782bad6501ea381a4c3586ff8b9648964
Jeffrey Carpenter <1329364+i8degrees@users.noreply.github.com>
2026-04-11 23:33:04 -0500

    Add `vendor/` dir to serve as git submodules

--- third-party/linux/sdl2_ttf.sh
+++ third-party/linux/sdl2_ttf.sh
@@ -10,20 +10,17 @@ if [ -n "$INIT_BUILD" ]; then
   popd
 fi
 
-SDL_LIBS="/mnt/fs1/Projects/nomlib.git.bck/third-party/linux/sdl2/lib/libSDL2.a"
-CPPFLAGS="/mnt/fs1/Projects/nomlib.git.bck/third-party/linux/sdl2/include/SDL2"
-SDL2_TTF_DEST=/mnt/fs1/Projects/nomlib.git.bck/third-party/linux/sdl2_ttf
-export SDL2_TTF_DEST
-
-if [ -n "$DEBUG" ]; then
-  export SDL_LIBS
-  export CPPFLAGS
-  export SDL2_TTF_DEST
-fi
+SDL_LIBS="/mnt/fs1/Projects/nomlib.git.fixed/third-party/linux/sdl2/lib/libSDL2.a"
+CPPFLAGS="/mnt/fs1/Projects/nomlib.git.fixed/third-party/linux/sdl2/include/SDL2"
+SDL2_TTF_DEST=/mnt/fs1/Projects/nomlib.git.fixed/third-party/linux/sdl2_ttf
+FREETYPE_PREFIX=/mnt/fs1/Projects/nomlig.git.fixed/third-party/linux/freetype
+export SDL2_TTF_DEST FREETYPE_PREFIX
 
 #cd /home/jeff/tmp/nomlib/sdl2_ttf.git
 #make clean
 ./autogen.sh
-./configure --prefix=$SDL2_TTF_DEST
-make install
+#./configure --prefix=$SDL2_TTF_DEST
+./configure --prefix="$SDL2_TTF_DEST" --with-freetype-prefix="$FREETYPE_PREFIX"
+make -j4
+make -j1 install
 

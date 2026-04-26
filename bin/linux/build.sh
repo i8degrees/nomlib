4f8157f3f8d7e353e6a2a645ddf58fd9040a83b9
Jeffrey Carpenter <1329364+i8degrees@users.noreply.github.com>
2026-04-26 17:57:28 -0500

    Update filesystem paths

--- bin/linux/build.sh
+++ bin/linux/build.sh
@@ -6,6 +6,6 @@ TMPDIR=$1
 mkdir -p $TMPDIR && cd $TMPDIR
 LOG_DIR=$TMPDIR/logs
 export LOG_DIR
-SRC=/mnt/fs1/Projects/nomlib.git
+SRC=/home/jeff/Projects/nomlib.git
 mkdir -p $LOG_DIR
-cmake -S $SRC -B $TMPDIR -DCMAKE_C_COMPILER=/usr/bin/clang -DCMAKE_CXX_COMPILER=/usr/bin/clang++ -DDEBUG=on -DDEBUG_ASSERT=on -DEXAMPLES=on -DNOM_BUILD_TESTS=on -DCMAKE_POLICY_VERSION_MINIMUM=3.5 -DGTEST_ROOT=/mnt/fs1/Projects/nomlib.git/third-party/linux/gtest -DGTEST_INCLUDE_DIR=/mnt/fs1/Projects/nomlib.git/third-party/linux/gtest/include -DGTEST_LIBRARY=/mnt/fs1/Projects/nomlib.git/third-party/linux/gtest/lib/libgtest.a
+cmake -S $SRC -B $TMPDIR -DCMAKE_C_COMPILER=/usr/bin/clang -DCMAKE_CXX_COMPILER=/usr/bin/clang++ -DDEBUG=on -DDEBUG_ASSERT=on -DEXAMPLES=on -DNOM_BUILD_TESTS=on -DCMAKE_POLICY_VERSION_MINIMUM=3.5 -DGTEST_ROOT="$SRC/third-party/linux/gtest" -DGTEST_INCLUDE_DIR="$SRC/third-party/linux/gtest/include" -DGTEST_LIBRARY="$SRC/third-party/linux/gtest/lib/libgtest.a"

5f9e8bb2bf3da6aaf72b69169f5ae13b240004e3
Jeffrey Carpenter <1329364+i8degrees@users.noreply.github.com>
2026-05-26 00:58:31 -0500

    Add vendor/ to git ignore

--- cmake/modules/PkgConfig.cmake
+++ cmake/modules/PkgConfig.cmake
@@ -10,48 +10,65 @@ set(CMAKE_INSTALL_LIBDIR "lib")
 set(CMAKE_INSTALL_INCLUDEDIR "include/nomlib")
 set(PROJECT_DESC "C++11 game engine")
 set(PROJECT_URL "https://github.com/i8degrees/nomlib")
-set(PROJECT_DEPS "SDL2 >=2.0.4 librocket >=1.3.0 libsndfile >=1.0.24 SDL2_image >=2.0.0 SDL2_ttf >= 2.0.12 openal >= 1.14")
-set(PROJECT_DEPS_PRIVATE "jsoncpp >= 1.x rapidxml >=1.x gtest >=1.8.x")
-set(PROJECT_LIBS "")
+set(PROJECT_DEPS "")
+# TODO(JEFF): The project dependency list should be modified to only reflect the
+# build units that are available, i.e.: without the audio unit, we do not
+# need the openal or (likely) sndfile libraries.
+set(PROJECT_DEPS "sdl2 >=2.0.4 SDL2_image >=2.0.0 sdl2_ttf >= 2.0.12 openal >= 1.14 libRocket >=1.3.0 sndfile >=1.0.24")
+set(PROJECT_DEPS_PRIVATE "")
+#set(PROJECT_DEPS_PRIVATE "jsoncpp >= 1.x rapidxml >=1.x gtest >=1.8.x")
+set(LIBS "")
+# Release builds will have no suffix.
+set(LIB_SUFFIX "")
+
+if(CMAKE_BUILD_TYPE MATCHES "debug/i")
+  set(LIB_SUFFIX "-d")
+endif(CMAKE_BUILD_TYPE MATCHES "debug/i")
+
+# DEPRECATED(JEFF): This "option" will be removed in a future release.
+# Favor the use of `CMAKE_BUILD_TYPE` instead.
+if(DEBUG)
+  set(LIB_SUFFIX "-d")
+endif(DEBUG)
 
 if(NOM_BUILD_ACTIONS_UNIT)
-  list( APPEND PROJECT_LIBS -l@PROJECT_NAME@-actions)
+  set(LIBS "${LIBS} -l${PROJECT_NAME}-actions${LIB_SUFFIX}")
 endif()
 
 if(NOM_BUILD_AUDIO_UNIT)
-  list( APPEND PROJECT_LIBS -l@PROJECT_NAME@-audio)
+  set(LIBS "${LIBS} -l${PROJECT_NAME}-audio${LIB_SUFFIX}")
 endif()
 
 if(NOM_BUILD_CORE_UNIT)
-  list( APPEND PROJECT_LIBS -l@PROJECT_NAME@-core)
+  set(LIBS "${LIBS} -l${PROJECT_NAME}-core${LIB_SUFFIX}")
 endif()
 
 if(NOM_BUILD_FILE_UNIT)
-  list( APPEND PROJECT_LIBS -l@PROJECT_NAME@-file)
+  set(LIBS "${LIBS} -l${PROJECT_NAME}-file${LIB_SUFFIX}")
 endif()
 
 if(NOM_BUILD_GRAPHICS_UNIT)
-  list( APPEND PROJECT_LIBS -l@PROJECT_NAME@-graphics)
+  set(LIBS "${LIBS} -l${PROJECT_NAME}-graphics${LIB_SUFFIX}")
 endif()
 
 if(NOM_BUILD_GUI_UNIT)
-  list( APPEND PROJECT_LIBS -l@PROJECT_NAME@-gui)
+  set(LIBS "${LIBS} -l${PROJECT_NAME}-gui${LIB_SUFFIX}")
 endif()
 
 if(NOM_BUILD_MATH_UNIT)
-  list( APPEND PROJECT_LIBS -l@PROJECT_NAME@-math)
+  set(LIBS "${LIBS} -l${PROJECT_NAME}-math${LIB_SUFFIX}")
 endif()
 
 if(NOM_BUILD_PTREE_UNIT)
-  list( APPEND PROJECT_LIBS -l@PROJECT_NAME@-ptree)
+  set(LIBS "${LIBS} -l${PROJECT_NAME}-ptree${LIB_SUFFIX}")
 endif()
 
 if(NOM_BUILD_SERIALIZERS_UNIT)
-  list( APPEND PROJECT_LIBS -l@PROJECT_NAME@-serializers)
+  set(LIBS "${LIBS} -l${PROJECT_NAME}-serializers${LIB_SUFFIX}")
 endif()
 
 if(NOM_BUILD_SYSTEM_UNIT)
-  list( APPEND PROJECT_LIBS -l@PROJECT_NAME@-system)
+  set(LIBS "${LIBS} -l${PROJECT_NAME}-system${LIB_SUFFIX}")
 endif()
 
 configure_file  ( "${CMAKE_TEMPLATE_PATH}/nomlib.pc.in"

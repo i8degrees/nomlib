2938ccd5637efd1ff82b41f6e43748688ae35853
Jeffrey Carpenter <1329364+i8degrees@users.noreply.github.com>
2026-06-06 02:37:56 -0500

    Initial commit - (experimental docker build scripts)

---
+++ bin/docker/user.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+#
+#
+#
+
+workspace=$1
+#workspace="$(pwd)"
+
+# (linux|macos|windows)
+if [ -z "$PLATFORMBUILD" ]; then
+  PLATFORMBUILD=amd64
+fi
+
+# docker env
+REPOSITORY=i8degrees/nomlib-builder
+VERSION=0.13.1
+ARCH=${PLATFORMBUILD}
+
+if [ -z "$workspace" ]; then
+  echo "Failed to run -- missing workspace path given to script!"
+  echo
+  exit 1
+fi
+
+# inside container paths
+VENDOR_PREFIX=/app/vendor
+WORKDIR="/home/$(id -un)"
+STAMP=$(date +%S) # displays in seconds 0..59
+USER_IDS=(-e BUILDER_UID="$( id -u )" -e BUILDER_GID="$( id -g )" -e BUILDER_USER="$( id -un )" -e BUILDER_GROUP="$( id -gn )")
+
+HOST_WORKDIR=$PWD
+SSH_DIR=
+if [ -z "${SSH_DIR}" ]; then
+  SSH_DIR="${HOME}/.ssh"
+fi
+
+echo $SSH_DIR
+
+HOST_VOLUMES+="-v $SSH_DIR:/home/$(id -un)/.ssh";
+#HOST_VOLUMES+="-v $HOST_WORKDIR:/app:rw";
+
+#-v $(pwd)/vendor:/app/vendor \
+docker run --rm -it \
+    --name nomlib-build-${STAMP} \
+    -v nomlib-libs:${VENDOR_PREFIX}:rw \
+    -v "${HOST_WORKDIR}":/app:rw \
+    -w "${WORKDIR}" \
+    $HOST_VOLUMES \
+    "${USER_IDS[@]}" \
+    --entrypoint /entrypoint.sh \
+    "${REPOSITORY}:${VERSION}-${ARCH}" \
+/bin/bash

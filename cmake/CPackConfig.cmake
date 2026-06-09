b8c58124647d42901cb96e7b348232dc7f59739a
Jeffrey Carpenter <1329364+i8degrees@users.noreply.github.com>
2026-06-09 00:06:07 -0500

    Update CPack deps, email and architecture env
    Create nomlib.desktop.in cmake template
    Remove cmake/packaging.cmake

--- cmake/CPackConfig.cmake
+++ cmake/CPackConfig.cmake
@@ -16,6 +16,10 @@ set ( CPACK_PACKAGE_VERSION "${PROJECT_VERSION_MAJOR}.${PROJECT_VERSION_MINOR}.$
 
 set ( CPACK_PACKAGE_FILE_NAME "${CPACK_PACKAGE_NAME}_${CPACK_PACKAGE_VERSION_MAJOR}.${CPACK_PACKAGE_VERSION_MINOR}.${CPACK_PACKAGE_VERSION_PATCH}" )
 
+set(CPACK_APPIMAGE_DESKTOP_FILE "${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}.desktop")
+#set(CPACK_APPIMAGE_DESKTOP_FILE "${CMAKE_TEMPLATE_PATH}/nomlib.desktop.in")
+#set(CPACK_PACKAGE_ICON "${PROJECT_SOURCE_DIR}/Resources/nomlib.png")
+
 # PackageMaker refuses to let us use the Markdown extension (MD) -_-
 #
 # TODO / Generate these text files on the fly from the Markdown sources
@@ -24,21 +28,27 @@ set ( CPACK_RESOURCE_FILE_LICENSE "${CMAKE_TEMPLATE_PATH}/LICENSE.html" )
 #set ( CPACK_RESOURCE_FILE_WELCOME "${CMAKE_TEMPLATE_PATH}/Welcome.txt" )
 #set ( CPACK_PACKAGE_DESCRIPTION_FILE "${CMAKE_TEMPLATE_PATH}/Description.txt" )
 
-set ( CPACK_PACKAGE_VENDOR "org.i8degrees.${PROJECT_NAME}" )
-set ( CPACK_PACKAGE_CONTACT "i8degrees@gmail.com" )
+set ( CPACK_PACKAGE_VENDOR "com.i8degrees.${PROJECT_NAME}" )
+set ( CPACK_PACKAGE_CONTACT "1329364+i8degrees@users.noreply.github.com")
 set ( CPACK_PACKAGE_DESCRIPTION "C++11 cross-platform game engine" )
 set ( CPACK_PACKAGE_DESCRIPTION_SUMMARY "nomlib - C++11 cross-platform game engine" )
 
 # TODO / verify these package dependency names
-set ( CPACK_DEBIAN_PACKAGE_DEPENDS "libsdl1.2 libsdl-image1.2 libsdl-ttf2.0 libsndfile1 libopenal libmagic libgtk2.0" )
+set ( CPACK_DEBIAN_PACKAGE_DEPENDS "libsdl2-2.0.0 libsdl2-image-2.0.0 libsdl2-ttf-2.0.0 libsndfile1 libopenal1")
 set ( CPACK_DEBIAN_PACKAGE_PRIORITY "optional" )
-set ( CPACK_DEBIAN_PACKAGE_SECTION "devel" )
-set ( CPACK_DEBIAN_PACKAGE_ARCHITECTURE "amd64" )
+set ( CPACK_DEBIAN_PACKAGE_SECTION "games" )
+set ( CPACK_DEBIAN_PACKAGE_ARCHITECTURE "${PLATFORM_ARCH}" )
 
-set ( CPACK_STRIP_FILES false )
+if(CMAKE_BUILD_TYPE MATCHES Rel)
+  set ( CPACK_STRIP_FILES true )
+endif()
 
 #set ( CPACK_INSTALL_PREFIX ${CMAKE_CURRENT_BINARY_DIR} )
 #set ( CPACK_PACKAGING_INSTALL_PREFIX ${CMAKE_INSTALL_PREFIX} )
 
 include(CPack)
 
+configure_file  ( "${CMAKE_TEMPLATE_PATH}/nomlib.desktop.in"
+                  "${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}.desktop"
+                  @ONLY
+                )

f7af78cf3f04c718907bfb2dc8e18af5961cec9a
Jeffrey Carpenter <1329364+i8degrees@users.noreply.github.com>
2026-07-02 23:28:43 -0500

    Improve docker build scripts
    Experimental devcontainer (untested)
    
    Both docker scripts now has bash history support
    
    The user docker script has a new docker volume added containing the latest
    project (The Makefile not yet written for this transfer)

--- bin/docker/build.sh
+++ bin/docker/build.sh
@@ -31,6 +31,7 @@ STAMP=$(date +%S) # displays in seconds 0..59
 docker run --rm -it \
     --name nomlib-build-${STAMP} \
     -v nomlib-libs:${VENDOR_PREFIX}:rw \
+    -v nomlib-bashhistory:/commandhistory:rw \
     -v "${workspace}":/app:rw \
     -w "${WORKDIR}" \
     --entrypoint /entrypoint.sh \
--- bin/docker/user.sh
+++ bin/docker/user.sh
@@ -15,21 +15,23 @@ fi
 REPOSITORY=i8degrees/nomlib-builder
 VERSION=0.13.1
 ARCH=${PLATFORMBUILD}
+HOST_WORKDIR="$(pwd)"
 
 if [ -z "$workspace" ]; then
-  HOST_VOLUMES+=("-v $HOST_WORKDIR:/app:rw");
+  HOST_VOLUMES+=("-v nomlib-project:/app:rw")
   #echo "Failed to run -- missing workspace path given to script!"
   #echo
   #exit 1
+else
+  HOST_VOLUMES+=("-v $HOST_WORKDIR:/app:rw");
 fi
 
 # inside container paths
-VENDOR_PREFIX=/app/vendor
+VENDOR_PREFIX=/app/vendor/prebuild
 WORKDIR="/home/$(id -un)"
 STAMP=$(date +%S) # displays in seconds 0..59
-USER_IDS=(-e BUILDER_UID="$( id -u )" -e BUILDER_GID="$( id -g )" -e BUILDER_USER="$( id -un )" -e BUILDER_GROUP="$( id -gn )")
+#USER_IDS=(-e BUILDER_UID="$( id -u )" -e BUILDER_GID="$( id -g )" -e BUILDER_USER="$( id -un )" -e BUILDER_GROUP="$( id -gn )")
 
-HOST_WORKDIR="$(pwd)"
 SSH_DIR=
 if [ -z "${SSH_DIR}" ]; then
   SSH_DIR="${HOME}/.ssh"
@@ -37,16 +39,18 @@ fi
 
 echo $SSH_DIR
 
-HOST_VOLUMES+=("-v $SSH_DIR:/home/$(id -un)/.ssh");
+#HOST_VOLUMES+=("-v $SSH_DIR:/home/$(id -un)/.ssh");
+HOST_VOLUMES+=("-v nomlib-libs:${VENDOR_PREFIX}:rw")
+HOST_VOLUMES+=("-v nomlib-bashhistory:/commandhistory:rw")
 
+    #--entrypoint /entrypoint.sh \
 #-v $(pwd)/vendor:/app/vendor \
 docker run --rm -it \
     --name nomlib-build-${STAMP} \
-    -v nomlib-libs:${VENDOR_PREFIX}:rw \
     -w "${WORKDIR}" \
-    $HOST_VOLUMES \
+    --user jeff \
+    ${HOST_VOLUMES[@]} \
     "${USER_IDS[@]}" \
-    --entrypoint /entrypoint.sh \
     "${REPOSITORY}:${VERSION}-${ARCH}" \
 /bin/bash
 # The first arg should be given instead of /bin/bash

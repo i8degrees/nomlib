2656bf11e2596eadc6e30ed42be62a92eb198e0b
Jeffrey Carpenter <1329364+i8degrees@users.noreply.github.com>
2026-07-15 11:16:31 -0500

    [WIP] @ai

---
+++ cmake/StandardProjectSettings.cmake
@@ -0,0 +1,69 @@
+# cmake/StandardProjectSettings.cmake:jeff
+#
+
+find_program(CCACHE ccache)
+if(CCACHE)
+  message(STATUS "Using ccache")
+  set(CMAKE_CXX_COMPILER_LAUNCHER ${CCACHE})
+else(NOT CCACHE)
+  message(STATUS "Ccache not found")
+endif()
+
+# nomlib began its life using c++14 with stdlib libc++ on MacOSX v10.9
+# (i386; x86_64) arch
+set(CMAKE_CXX_STANDARD 14)
+set(CMAKE_CXX_STANDARD_REQUIRED ON)
+set(CMAKE_CXX_EXTENSIONS OFF)
+
+# Set a default build type if none was specified
+if(NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES)
+  message(STATUS "Setting build type to 'Debug' as none was specified.")
+  set(CMAKE_BUILD_TYPE Debug CACHE STRING "Choose the type of build." FORCE)
+
+  # Set the possible values of build type for cmake-gui, ccmake
+  set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS "Debug" "Release" "RelWithDebInfo")
+endif()
+
+if(CMAKE_BUILD_TYPE MATCHES Debug OR CMAKE_BUILD_TYPE MATCHES RelWithDebInfo)
+  set(CMAKE_VERBOSE_MAKEFILE ON)
+  add_compile_definitions("NOM_DEBUG")
+endif()
+
+#option(DEBUG "Enable debug statements and asserts" OFF)
+#if(DEBUG OR CMAKE_BUILD_TYPE STREQUAL "Debug")
+#add_compile_definitions(DEBUG APP_PROFILE)
+#endif()
+
+if(DEBUG_ASSERT)
+  # Enable NOM_ASSERT macros
+  add_compile_definitions("NOM_DEBUG_ASSERT")
+else(NOT DEBUG_ASSERT)
+  # Disable NOM_ASSERT macros
+  add_compile_definitions("NDEBUG")
+endif()
+
+
+# Generate compile_commands.json to make it easier to work with clang based 
+# tools
+set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
+
+# cmake option NOLOG - disable engine logging
+option(NOLOG "Disable logging" OFF)
+if(NOLOG)
+  # TODO(JEFF): This needs to be implemented in config.hpp.in
+  add_compile_definitions("NOLOG")
+endif()
+
+# Support building nomlib as a DLL shared library on MS Windows; the
+# following properties ensure that all library targets built will have the
+# right symbol visibility in conjunction with the use of the internal
+# NOM_EXPORT macros that wrap the necessary __declspec(dllexport) declarations.
+if(WIN32 OR HIDE_SYMBOL_VISIBILITY)
+  if(NOT DEFINED CMAKE_CXX_VISIBILITY_PRESET AND
+    NOT DEFINED CMAKE_VISIBILITY_INLINES_HIDDEN)
+    set(CMAKE_CXX_VISIBILITY_PRESET hidden)
+    set(CMAKE_VISIBILITY_INLINES_HIDDEN YES)
+  endif()
+endif(WIN32 OR HIDE_SYMBOL_VISIBILITY)
+
+message(STATUS "Generating CMake project files with ${CMAKE_GENERATOR}")
---
+++ cmake/StaticAnalyzers.cmake
@@ -0,0 +1,25 @@
+# StaticAnalyzers.cmake:jeff
+#
+
+if(NOT CMAKE_BUILD_TYPE STREQUAL "Release")
+  find_program(CLANGTIDY clang-tidy)
+  if(CLANGTIDY)
+    message(STATUS "Using clang-tidy, found ${CLANGTIDY}")
+    set(CMAKE_CXX_CLANG_TIDY "${CLANGTIDY};-extra-arg=-Wno-unknown-warning-option")
+    # Explicitly enable exceptions on Windows
+    if(WIN32)
+      set(CMAKE_CXX_CLANG_TIDY "${CMAKE_CXX_CLANG_TIDY};--extra-arg=/EHsc")
+    endif()
+  else()
+    message(WARNING "clang-tidy requested but executable not found")
+  endif()
+
+  # This will gradually increase memory usage of the program,
+  # discovered on Apple M1, 13.0.
+  if(NOT WIN32)
+    message(STATUS "Using address sanitizer")
+    add_compile_options(-O0 -fsanitize=address -g)
+    add_link_options(-O0 -fsanitize=address -g)
+  endif()
+endif()
+

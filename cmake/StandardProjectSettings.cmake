866a7bd6ae97e773eefb9818733dacc6bc54f230
Jeffrey Carpenter <1329364+i8degrees@users.noreply.github.com>
2026-07-17 00:01:30 -0500

    Replace custom platform macros with standard CMake variables and add ccache/distcc options
    - remove `cmake/platform.cmake` - cmake `NOM_PLATFORM` variables are gone and I have updated the build system to use cmake's built-in matchers, i.e.: UNIX, WIN32 && APPLE
    
    - Fix the lack of test resources being installed under `opt/Resources` when the project target `install` is executed

--- cmake/StandardProjectSettings.cmake
+++ cmake/StandardProjectSettings.cmake
@@ -1,12 +1,25 @@
 # cmake/StandardProjectSettings.cmake:jeff
 #
 
-find_program(CCACHE ccache)
-if(CCACHE)
-  message(STATUS "Using ccache")
-  set(CMAKE_CXX_COMPILER_LAUNCHER ${CCACHE})
-else(NOT CCACHE)
-  message(STATUS "Ccache not found")
+if(DEFINED NOM_USE_CCACHE)
+  find_program(CCACHE ccache)
+  if(CCACHE)
+    message(STATUS "Using ccache")
+    set(CMAKE_CXX_COMPILER_LAUNCHER ${CCACHE})
+  else(NOT CCACHE)
+    message(STATUS "Ccache not found")
+  endif()
+endif()
+
+# EXPERIMENTAL(JEFF): This is a WIP and not ready for general consumption.
+if(DEFINED NOM_USE_DISTCC)
+  find_program(DISTCC distcc)
+  if(DISTCC)
+    message(STATUS "Using distcc")
+    set(CMAKE_CXX_COMPILER_LAUNCHER "${CMAKE_CXX_COMPILER_LAUNCHER};${DISTCC}")
+  else(NOT DISTCC)
+    message(STATUS "distcc not found")
+  endif(DISTCC)
 endif()
 
 # nomlib began its life using c++14 with stdlib libc++ on MacOSX v10.9
@@ -18,39 +31,37 @@ set(CMAKE_CXX_EXTENSIONS OFF)
 # Set a default build type if none was specified
 if(NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES)
   message(STATUS "Setting build type to 'Debug' as none was specified.")
-  set(CMAKE_BUILD_TYPE Debug CACHE STRING "Choose the type of build." FORCE)
+  set(CMAKE_BUILD_TYPE "Debug" CACHE STRING "Choose the type of build." FORCE)
 
   # Set the possible values of build type for cmake-gui, ccmake
-  set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS "Debug" "Release" "RelWithDebInfo")
+  set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS "Debug" "Release" "RelWithDebInfo" "MinSizeRel")
 endif()
 
-if(CMAKE_BUILD_TYPE MATCHES Debug OR CMAKE_BUILD_TYPE MATCHES RelWithDebInfo)
+if(CMAKE_BUILD_TYPE MATCHES "Debug" OR CMAKE_BUILD_TYPE MATCHES "RelWithDebInfo")
   set(CMAKE_VERBOSE_MAKEFILE ON)
   add_compile_definitions("NOM_DEBUG")
+  message(STATUS "[NOM_DEBUG] Debugging macros and logging activated.")
 endif()
-
-#option(DEBUG "Enable debug statements and asserts" OFF)
-#if(DEBUG OR CMAKE_BUILD_TYPE STREQUAL "Debug")
-#add_compile_definitions(DEBUG APP_PROFILE)
-#endif()
 
 if(DEBUG_ASSERT)
   # Enable NOM_ASSERT macros
   add_compile_definitions("NOM_DEBUG_ASSERT")
+  message(STATUS "[NOM_DEBUG_ASSERT] Run-time assertions are enabled.")
 else(NOT DEBUG_ASSERT)
   # Disable NOM_ASSERT macros
   add_compile_definitions("NDEBUG")
+  message(STATUS "[NDEBUG] Disabling run-time assertions")
 endif()
 
-
-# Generate compile_commands.json to make it easier to work with clang based 
+# Generate compile_commands.json to make it easier to work with clang based
 # tools
 set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
 
 # cmake option NOLOG - disable engine logging
 option(NOLOG "Disable logging" OFF)
 if(NOLOG)
-  # TODO(JEFF): This needs to be implemented in config.hpp.in
+  # TODO(JEFF): This needs to be implemented in config.hpp.in;
+  # rewrap our logging macros
   add_compile_definitions("NOLOG")
 endif()
 

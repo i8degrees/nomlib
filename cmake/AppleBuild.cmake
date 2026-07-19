76a1206c8d16b52e931b285d6d7b4d30dc50506d
Jeffrey Carpenter <1329364+i8degrees@users.noreply.github.com>
2026-07-18 22:58:53 -0500

    [WIP] @ai

---
+++ cmake/AppleBuild.cmake
@@ -0,0 +1,49 @@
+# cmake/AppleBuild.cmake:jeff
+#
+# This file needs to be included before calling `project`.
+
+if(APPLE AND "${CMAKE_GENERATOR}" STREQUAL "Xcode")
+  # This variable influences the system header files version we build against,
+  # which in turn determines the **minimum** version of OS X this build will
+  # be binary compatible with (run on).
+  #
+  # Internally, this influences the search paths for system header files and
+  # also instructs CMake to pass -mmacosx-version-min to the compiler.
+  #
+  # To modify this variable's resulting value, you should pass
+  # -DCMAKE_OSX_DEPLOYMENT_TARGET=<ver> to CMake at the time of project files
+  # generation -- 'cmake ..' from your out-of-source build directory.
+  #
+  # NOTE: CMAKE_OSX_SYSROOT must be set **before** the project command is
+  # called.
+  if(NOT CMAKE_OSX_SYSROOT)
+    execute_process(
+        COMMAND xcrun --show-sdk-path
+        OUTPUT_VARIABLE DETECTED_SDK_PATH
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+    execute_process(
+        COMMAND xcrun --show-sdk-version
+        OUTPUT_VARIABLE DETECTED_SDK_VERSION
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+    set(CMAKE_OSX_SYSROOT ${DETECTED_SDK_PATH} CACHE PATH "macOS SDK Path" FORCE)
+    set(NOM_MACOSX_SDK_VERSION ${DETECTED_SDK_VERSION} CACHE PATH "macOS SDK Version" FORCE)
+
+    message(STATUS "Using macOS SDK at ${CMAKE_OSX_SYSROOT}")
+    message(STATUS "Using macOS SDK version ${NOM_MACOSX_SDK_VERSION}")
+  endif(NOT CMAKE_OSX_SYSROOT)
+
+  # Define apple architecture for Release builds, use default. For an explicit
+  # universal executable use `x86_64;arm64`.
+  set(CMAKE_OSX_ARCHITECTURES "$(ARCHS_STANDARD)" CACHE INTERNAL "OS X architecture")
+
+  # Support older macOS versions.
+  set(CMAKE_OSX_DEPLOYMENT_TARGET 10.15 CACHE STRING "Minimum OS X deployment version")
+
+  if(BUILD_UNIVERSAL)
+    set(CMAKE_OSX_ARCHITECTURES "x86_64;arm64")
+  endif(BUILD_UNIVERSAL)
+
+endif(APPLE AND "${CMAKE_GENERATOR}" STREQUAL "Xcode")
+
--- cmake/platform.cmake
+++ cmake/platform.cmake
@@ -1,135 +1,22 @@
-# Platform detection script for CMake
-
-# Always use SDL2's enhanced assert facilities.
+# cmake/platform.cmake:jeff
 #
-# See also: include/nomlib/config.hpp.in
-set( NOM_USE_SDL2_ASSERT true )
+# Platform detection script for use in CMake & C preprocessor macros defined
+# elsewhere
 
-#
-## Platform architecture optimizations and targets
-#
-
-option(ARCH_32 "Build target for 32-bit CPU" OFF)
-option(ARCH_64 "Build target for 64-bit CPU" ON)
-option(AARCH_32 "Build target ARM CPU" OFF)
-option(AARCH_64 "Build target ARM64 CPU" OFF)
-
-if ( CMAKE_SYSTEM_NAME STREQUAL "Darwin" )
+if(CMAKE_SYSTEM_NAME STREQUAL "Darwin")
   # TODO: Rename to NOM_PLATFORM_OSX
-  set( PLATFORM_OSX true )
-  set( NOM_PLATFORM_POSIX true )
+  set(PLATFORM_OSX TRUE)
+  set(NOM_PLATFORM_OSX TRUE)
+  set(NOM_PLATFORM_POSIX TRUE)
+endif()
 
-  # Platform-specific options; the following options are applicable when the
-  # platform is Darwin (PLATFORM_OSX).
-  # TODO: Rename to BUILD_FRAMEWORK
-  option(FRAMEWORK "Build OSX Framework instead of dylib" ON)
-  option(UNIVERSAL "Build as an OSX Universal Library" OFF) # multi-arch
+if(CMAKE_SYSTEM_NAME STREQUAL "Linux")
+  set(PLATFORM_LINUX TRUE)
+  set(NOM_PLATFORM_POSIX TRUE)
+  set(PLATFORM_POSIX TRUE)
+endif()
 
-  # FIXME(JEFF): Refactor platform architectures section; universal binaries
-  # are NOT a MacOSX specific feature!
-  if(UNIVERSAL)
-    set(CMAKE_OSX_ARCHITECTURES i386; x86_64)
-    set(PLATFORM_ARCH "x86; x64") # Reserved for future use
-  else()
-    set(CMAKE_OSX_ARCHITECTURES x86_64)
-    set(PLATFORM_ARCH "x64") # Reserved for future use
-  endif(UNIVERSAL)
-
-  # This variable influences the system header files version we build against,
-  # which in turn determines the **minimum** version of OS X this build will
-  # be binary compatible with (run on).
-  #
-  # Internally, this influences the search paths for system header files and
-  # also instructs CMake to pass -mmacosx-version-min to the compiler.
-  #
-  # To modify this variable's resulting value, you should pass
-  # -DCMAKE_OSX_DEPLOYMENT_TARGET=<ver> to CMake at the time of project files
-  # generation -- 'cmake ..' from your out-of-source build directory.
-  #
-  # NOTE: CMAKE_OSX_SYSROOT must be set **before** the project command is
-  # called.
-  if(NOT CMAKE_OSX_SYSROOT)
-    execute_process(
-        COMMAND xcrun --show-sdk-path
-        OUTPUT_VARIABLE DETECTED_SDK_PATH
-        OUTPUT_STRIP_TRAILING_WHITESPACE
-    )
-    execute_process(
-        COMMAND xcrun --show-sdk-version
-        OUTPUT_VARIABLE DETECTED_SDK_VERSION
-        OUTPUT_STRIP_TRAILING_WHITESPACE
-    )
-    set(CMAKE_OSX_SYSROOT ${DETECTED_SDK_PATH} CACHE PATH "macOS SDK Path" FORCE)
-    set(NOM_MACOSX_SDK_VERSION ${DETECTED_SDK_VERSION} CACHE PATH "macOS SDK Version" FORCE)
-
-    message(STATUS "Using macOS SDK at ${CMAKE_OSX_SYSROOT}")
-    message(STATUS "Using macOS SDK version ${NOM_MACOSX_SDK_VERSION}")
-  endif(NOT CMAKE_OSX_SYSROOT)
-
-  # libc++ requires OSX v10.7+
-  set ( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11 -stdlib=libc++" )
-
-  if ( CMAKE_GENERATOR STREQUAL "Xcode" )
-    set ( CMAKE_XCODE_ATTRIBUTE_CLANG_CXX_LANGUAGE_STANDARD "c++11" )
-    set ( CMAKE_XCODE_ATTRIBUTE_CLANG_CXX_LIBRARY "libc++" )
-  endif ( CMAKE_GENERATOR STREQUAL "Xcode" )
-
-  message ( STATUS "Platform: Darwin (Mac OS X)" )
-elseif ( CMAKE_SYSTEM_NAME STREQUAL "Linux" ) # Tested on Ubuntu v12.04-LTS
-  set( PLATFORM_LINUX true )
-  # DEPRECATED(JEFF): Use `PLATFORM_POSIX` instead as `NOM_PLATFORM_POSIX`
-  # does not fit with the other platform variables we define. We must update
-  # our codebase to not use the following before we can remove it!
-  set( NOM_PLATFORM_POSIX true )
-  set( PLATFORM_POSIX true )
-
-  # TODO(JEFF): Create build time option to override compilation tooling
-  # between clang and probably even GNU GCC, once we find the right C++
-  # variant for it...
-  #
-  # Default to clang based tooling when found...
-  if ( CMAKE_CXX_COMPILER MATCHES "clang" )
-    message ( STATUS "Using clang based platform to build..." )
-    #set ( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++14" )
-    # nomlib began its life under c++14 on Intel Darwin Mac OSX
-    # >> Modern GTest build requires a c++17 minimum
-    set(CMAKE_CXX_STANDARD 14)
-    set(CMAKE_CXX_STANDARD_REQUIRED ON)
-    set(CMAKE_CXX_EXTENSIONS OFF)
-    # libc++ requires OSX v10.7+
-    #set ( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++14 -stdlib=libc++" )
-  elseif ( CMAKE_C_COMPILER MATCHES "gcc" )
-    message ( STATUS "Using gcc based platform to build..." )
-    message ( WARNING "nomlib only supports building with clang." )
-
-    # !! GoogleTest unit testing framework v1.10.x requires a minimum C++ level 11
-    set(CMAKE_CXX_STANDARD 11)
-    set(CMAKE_CXX_STANDARD 14)
-    set ( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++0x" )
-  endif( CMAKE_CXX_COMPILER MATCHES "clang" )
-
-  message ( STATUS "Platform: Linux" )
-  message ( STATUS "Build platform: ${CMAKE_CXX_COMPILER}" )
-  message ( STATUS "Compiler C++ level: ${CMAKE_CXX_STANDARD}" )
-  message ( STATUS "Compiler flags: ${CMAKE_CXX_FLAGS}" )
-elseif ( CMAKE_SYSTEM_NAME STREQUAL "Windows" )
-  set( PLATFORM_WINDOWS true )
-
-  message ( STATUS "Platform: Windows" )
-else () # Not Linux nor OSX
-
-  set ( PLATFORM_UNKNOWN true )
-  message ( STATUS "Platform: Unknown" )
-
-endif ( CMAKE_SYSTEM_NAME STREQUAL "Darwin" )
-
-# FIXME(JEFF): Refactor platform architectures
-if ( PLATFORM_WINDOWS AND ARCH_32 )
-  set ( PLATFORM_ARCH "x86" )
-elseif ( PLATFORM_WINDOWS AND ARCH_64 )
-  set ( PLATFORM_ARCH "x64" )
-endif ( PLATFORM_WINDOWS AND ARCH_32 )
-
-# nomlib.pc.in -> nomlib.pc
-include(PkgConfig)
-
+if(CMAKE_SYSTEM_NAME STREQUAL "Windows")
+  set(PLATFORM_WINDOWS TRUE)
+  set(PLATFORM_WIN TRUE)
+endif()

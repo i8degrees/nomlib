33155d47127ec6aa78ebe9cec0c0ffcf63500448
Jeffrey Carpenter <1329364+i8degrees@users.noreply.github.com>
2026-08-19 18:33:09 -0500

    (macos) Copy nomlib framework deps to app bundles
    This resolves all but one remaining issue with the examples on
    MacOS. The final remaining issue now is dealing with the copying
    of the resource data files into its right place.

--- cmake/functions.cmake
+++ cmake/functions.cmake
@@ -47,3 +47,58 @@ endfunction()
 #function(install_dependency spath dest comp)
   #install(FILES ${spath} DESTINATION ${dest} COMPONENT comp)
 #endfunction()
+
+# \brief Perform a copy of the target framework to its final destination
+# (app bundle). This function is a NO-OP when the platform is not APPLE
+# **and** CMAKE_FRAMEWORK has not been defined.
+#
+# \extra The public headers in each framework bundle are not (yet) present at
+# the time of executing this function, and thus can only be used for run-time
+# dependencies (not development).
+#
+# \param target - The executable target to copy to
+# \param lib - One or more libraries to copy
+function(copy_framework_dep target)
+  if(NOT APPLE AND NOT DEFINED CMAKE_FRAMEWORK)
+    #message(FATAL_ERROR
+      #"Apple and CMAKE_FRAMEWORK must be defined")
+    return()
+  endif()
+
+  foreach(library ${ARGN})
+    add_custom_command(
+      TARGET ${target} POST_BUILD
+      COMMAND ${CMAKE_COMMAND} -E make_directory "$<TARGET_FILE_DIR:${target}>/../Frameworks"
+      COMMAND ${CMAKE_COMMAND} -E copy_directory
+        "${CMAKE_BINARY_DIR}/lib/${library}.framework"
+      "$<TARGET_FILE_DIR:${target}>/../Frameworks/${library}.framework"
+      # ensure that arguments with spaces or special chars handle expansion
+      # safely alongside generator expressions
+      VERBATIM
+    )
+    # FIXME(JEFF): The generator expression version is ideal as the current
+    # command above will fail if a multi-config generator project were to be
+    # used.
+    #add_custom_command(
+      #TARGET ${target} POST_BUILD
+      #COMMAND $<$:$<STREQUAL:$<PLATFORM_ID>,Darwin>:${CMAKE_COMMAND}> -E make_directory "$<TARGET_FILE_DIR:${target}>/../Frameworks"
+      #COMMAND $<$:$<STREQUAL:$<PLATFORM_ID>,Darwin>:${CMAKE_COMMAND}> -E copy_directory
+      #"${CMAKE_BINARY_DIR}/lib/${library}.framework"
+        #"$<TARGET_FILE_DIR:${target}>/../Frameworks/${library}.framework"
+        #VERBATIM
+      #)
+  endforeach()
+endfunction()
+
+# Set target's @rpath
+#
+# \param target - library or executable
+# \param path - The run-time path to set on the target upon install (redist).
+function(set_rpath target path)
+  set_target_properties("${target}"
+    PROPERTIES
+      INSTALL_RPATH "${path}"
+      INSTALL_RPATH_USE_LINK_PATH TRUE
+  )
+endfunction()
+

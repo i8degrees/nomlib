e16a226db719c797432c0306aa64d967f4de134b
Jeffrey Carpenter <1329364+i8degrees@users.noreply.github.com>
2026-08-22 00:16:26 -0500

    Fix ulong static_assert

--- include/nomlib/types.hpp
+++ include/nomlib/types.hpp
@@ -296,7 +296,7 @@ static_assert ( sizeof ( nom::uchar ) == 1, "nom::uchar" );
 #if defined(NOM_PLATFORM_ARCH_X86_64)
 
 #if defined(__WIN32__)
-  static_assert( sizeof ( nom::ulong ) == 8, "nom::ulong" );
+  static_assert( sizeof ( nom::ulong ) == 4, "nom::ulong" );
   static_assert( sizeof ( nom::size_type ) == 8, "nom::size_type" );
 #else
   static_assert( sizeof ( nom::ulong ) == 4, "nom::ulong" );

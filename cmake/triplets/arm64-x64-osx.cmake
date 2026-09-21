e9b12bb7090b73abff797d46c606b2f3cbd4fad9
Jeffrey Carpenter <1329364+i8degrees@users.noreply.github.com>
2026-09-21 16:40:33 -0500

    WIP - arm64 x64-osx port

--- cmake/triplets/arm64-x64-osx.cmake
+++ cmake/triplets/arm64-x64-osx.cmake
@@ -7,3 +7,28 @@ set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE static)
 set(VCPKG_CMAKE_SYSTEM_NAME Darwin)
 set(VCPKG_OSX_ARCHITECTURES "arm64;x86_64")
+
+# Port-specific fixes for universal binary compilation
+if(PORT STREQUAL "libpng")
+  # Disable hardware-specific SIMD instructions (like ARM Neon)
+  # so both the x86_64 and arm64 slices can compile successfully from a
+  # single CMake config.
+  list(APPEND VCPKG_CMAKE_CONFIGURE_OPTIONS "-DPNG_HARDWARE_OPTIMIZATIONS=OFF")
+endif(PORT STREQUAL "libpng")
+
+# Port-specific fixes for universal binary compilation
+if(PORT STREQUAL "liblzma")
+  # Disable hardware-specific SIMD instructions (like ARM Neon)
+  # so both the x86_64 and arm64 slices can compile successfully from a
+  # single CMake config.
+  list(APPEND VCPKG_CMAKE_CONFIGURE_OPTIONS "-DENABLE_ASSEMBLER=OFF")
+endif(PORT STREQUAL "liblzma")
+
+# Port-specific fixes for universal binary compilation
+if(PORT STREQUAL "libjpeg-turbo")
+  # Disable hardware-specific SIMD instructions (like ARM Neon)
+  # so both the x86_64 and arm64 slices can compile successfully from a
+  # single CMake config.
+  # list(APPEND VCPKG_CMAKE_CONFIGURE_OPTIONS "-DCMAKE_ASM_FLAGS=--target=arm64-osx")
+  list(APPEND VCPKG_CMAKE_CONFIGURE_OPTIONS "-DWITH_SIMD=0" "-DCMAKE_C_FLAGS=--target=arm64-apple-macos" "-DCMAKE_OSX_ARCHITECTURES=arm64")
+endif(PORT STREQUAL "libjpeg-turbo")

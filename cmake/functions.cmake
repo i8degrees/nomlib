5bfe89b3fed44962fb84dfe4611603b6bdeaba74
Jeffrey Carpenter <1329364+i8degrees@users.noreply.github.com>
2026-09-22 02:35:06 -0500

    WIP - new macos framework functions

--- cmake/functions.cmake
+++ cmake/functions.cmake
@@ -103,3 +103,50 @@ function(set_rpath target path)
   )
 endfunction()
 
+# Setup a MacOS Framework bundle for the specified (library) target.
+#
+# \param target_name - the target library to set properties on.
+# \param template - the template file is input to use for creating the
+# Info.plist.
+#
+# \note This function is a NO-OP on non-APPLE platforms.
+function(apple_setup_framework target_name template)
+  set_target_properties(${target_name}
+    PROPERTIES
+      FRAMEWORK TRUE
+      MACOSX_FRAMEWORK_INFO_PLIST
+        ${template}
+      MACOSX_FRAMEWORK_NAME
+        "${target_name}"
+      MACOSX_FRAMEWORK_BUNDLE_VERSION
+        "${PROJECT_VERSION_MAJOR}.${PROJECT_VERSION_MINOR}.${PROJECT_VERSION_PATCH}-${CMAKE_BUILD_TYPE}"
+      MACOSX_FRAMEWORK_SHORT_VERSION_STRING
+        "${PROJECT_VERSION_MAJOR}.${PROJECT_VERSION_MINOR}"
+      MACOSX_FRAMEWORK_IDENTIFIER
+        "net.i8degrees.${target_name}"
+      XCODE_ATTRIBUTE_CODE_SIGN_IDENTITY
+        ""
+      MACOSX_RPATH TRUE
+  )
+endfunction()
+
+# MacOSX install framework headers for the specified (library) target
+#
+# \param target - the target library to copy the public header files to
+# `target.framework/Headers`.
+#
+# \note This function is a NO-OP unless the platform branch is APPLE
+# (cmake built-in variable)
+macro(apple_install_framework_headers target_name)
+  if(APPLE)
+    install(TARGETS "${target_name}"
+      EXPORT libtarget
+      # ~/Library/Frameworks/target.framework
+      FRAMEWORK DESTINATION "${CMAKE_INSTALL_PREFIX}"
+        COMPONENT Runtime
+        FILE_SET HEADERS DESTINATION "${CMAKE_INSTALL_PREFIX}/${target_name}.framework/Headers"
+        COMPONENT Development
+    )
+  endif(APPLE)
+  #endfunction()
+endmacro()
